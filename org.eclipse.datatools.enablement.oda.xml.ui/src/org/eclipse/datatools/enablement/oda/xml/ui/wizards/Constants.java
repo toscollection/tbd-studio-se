00a1a69cfc24dd2829944ae02f624054b2a62670
cantoine <cantoine@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-10-31 09:05:38 +0000

    DTD project plugin added to trunk
    git-svn-id: http://talendforge.org/svn/tos/trunk@267 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.eclipse.datatools.enablement.oda.xml.ui/src/org/eclipse/datatools/enablement/oda/xml/ui/wizards/Constants.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.datatools.enablement.oda.xml.ui.wizards;
+
+/**
+ * Contants for xml
+ */
+
+public class Constants
+{
+	// used by oda , saved in dataSouce
+	public static final String CONST_PROP_FILELIST = "FILELIST";
+	public static final String CONST_PROP_SCHEMA_FILELIST="SCHEMAFILELIST";
+	
+	// saved in dataset
+	public static final String CONST_PROP_RELATIONINFORMATION = "RELATIONINFORMATION";
+	public static final String CONST_PROP_XML_FILE = "XML_FILE";
+	public static final String CONST_PROP_MAX_ROW = "MAX_ROW";
+	public static final String CONST_PROP_XPATH = "XPATHTEXT";
+	public static final String CONST_PROP_TABLE_NAME ="TABLENAME";
+
+}
