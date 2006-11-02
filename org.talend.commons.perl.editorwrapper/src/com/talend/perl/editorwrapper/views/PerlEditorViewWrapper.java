4a8db3b19246e926ca81b3ddfa150fe66cfb2830
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-11-02 13:01:44 +0000

    Format
    git-svn-id: http://talendforge.org/svn/tos/trunk@300 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.perl.editorwrapper/src/com/talend/perl/editorwrapper/views/PerlEditorViewWrapper.java
+++ org.talend.commons.perl.editorwrapper/src/com/talend/perl/editorwrapper/views/PerlEditorViewWrapper.java
@@ -35,18 +35,17 @@ import org.eclipse.ui.part.FileEditorInput;
 import org.epic.perleditor.editors.PerlEditor;
 
 /**
- * DOC ccarbone  class global comment. Detailled comment
- * <br/>
- *
+ * DOC ccarbone class global comment. Detailled comment <br/>
+ * 
  * $Id$
- *
+ * 
  */
 public class PerlEditorViewWrapper extends PerlEditor implements IViewPart {
 
-	private IViewSite viewSite;
-	private IEditorSite editorSite;
-	
-	
+    private IViewSite viewSite;
+
+    private IEditorSite editorSite;
+
     /**
      * Creates a new view.
      */
@@ -54,98 +53,100 @@ public class PerlEditorViewWrapper extends PerlEditor implements IViewPart {
         super();
     }
 
-    /* (non-Javadoc)
-     * Method declared on IViewPart.
+    /*
+     * (non-Javadoc) Method declared on IViewPart.
      */
     public IViewSite getViewSite() {
         return viewSite;
     }
 
-    
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.eclipse.ui.IViewPart#init(org.eclipse.ui.IViewSite)
      */
     public void init(IViewSite site) throws PartInitException {
         viewSite = site;
         editorSite = new PerlEditorSite(site);
         setSite(editorSite);
-	
+
         init(editorSite, new FileEditorInput(getSamplePerlFile()));
     }
-    
-	private IFile getSamplePerlFile() {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IFile perlFile = workspace.getRoot().getProject("TestPerl").getFile("test.pl");
-		return perlFile;
-	}    
 
-@Override
-    public IWorkbenchPartSite getSite() {
-    	// TODO Auto-generated method stub
-    	return viewSite;
+    private IFile getSamplePerlFile() {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IFile perlFile = workspace.getRoot().getProject("TestPerl").getFile("test.pl");
+        return perlFile;
     }
 
-@Override
-	public IEditorSite getEditorSite() {
-	// TODO Auto-generated method stub
-	return editorSite;
-}
-    
-    /* 
+    @Override
+    public IWorkbenchPartSite getSite() {
+        // TODO Auto-generated method stub
+        return viewSite;
+    }
+
+    @Override
+    public IEditorSite getEditorSite() {
+        // TODO Auto-generated method stub
+        return editorSite;
+    }
+
+    /*
      * (non-Javadoc)
+     * 
      * @see org.eclipse.ui.IViewPart#init(org.eclipse.ui.IViewSite, org.eclipse.ui.IMemento)
      */
     public void init(IViewSite site, IMemento memento) throws PartInitException {
-    	/*
-    	* Initializes this view with the given view site.  A memento is passed to
-        * the view which contains a snapshot of the views state from a previous
-        * session.  Where possible, the view should try to recreate that state
-        * within the part controls.
-        * <p>
-        * This implementation will ignore the memento and initialize the view in
-        * a fresh state.  Subclasses may override the implementation to perform any
-        * state restoration as needed.
-        */
+        /*
+         * Initializes this view with the given view site. A memento is passed to the view which contains a snapshot of
+         * the views state from a previous session. Where possible, the view should try to recreate that state within
+         * the part controls. <p> This implementation will ignore the memento and initialize the view in a fresh state.
+         * Subclasses may override the implementation to perform any state restoration as needed.
+         */
         init(site);
     }
 
-  
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.eclipse.ui.IViewPart#saveState(org.eclipse.ui.IMemento)
      */
     public void saveState(IMemento memento) {
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.eclipse.ui.part.WorkbenchPart#setPartName(java.lang.String)
      */
     protected void setPartName(String partName) {
         super.setPartName(partName);
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.eclipse.ui.part.WorkbenchPart#setContentDescription(java.lang.String)
      */
     protected void setContentDescription(String description) {
         super.setContentDescription(description);
     }
 
-    /* (non-Javadoc)
-     * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement, java.lang.String, java.lang.Object)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement,
+     * java.lang.String, java.lang.Object)
      */
-    public void setInitializationData(IConfigurationElement cfig,
-            String propertyName, Object data) {
+    public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data) {
         super.setInitializationData(cfig, propertyName, data);
 
     }
 
-
-    
     @Override
     protected void createActions() {
-    	// TODO Auto-generated method stub
-    	super.createActions();
+        // TODO Auto-generated method stub
+        super.createActions();
     }
-    
-  
+
 }
