fea8ec472b147eb14d95ae9952c6bf5c75f4523b
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-01-26 11:43:02 +0000

    feature 495 modification: The modules view now supports for a new display, only need replace the code on line 72 in class ModulesView to a new composite.
    git-svn-id: http://talendforge.org/svn/tos/trunk@1613 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.codegen.perlmodule/src/main/java/org/talend/designer/codegen/perlmodule/ui/views/IModulesViewComposite.java
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.designer.codegen.perlmodule.ui.views;
+
+/**
+ * The composite within the modules view should implemented this interface.
+ * 
+ * yzhang class global comment. Detailled comment <br/>
+ * 
+ * $Id: IModulesViewComposite.java IModulesViewComposite 2007-1-26 下午04:41:33 +0000 (下午04:41:33, 2007-1-26 2007) yzhang $
+ * 
+ */
+public interface IModulesViewComposite {
+
+    /**
+     * Refresh the content of this composite, if the button refresh which on the local toolbar of moudles view clicked.
+     * 
+     * yzhang Comment method "refresh".
+     */
+    public void refresh();
+
+}
--- org.talend.designer.codegen.perlmodule/src/main/java/org/talend/designer/codegen/perlmodule/ui/views/ModulesView.java
+++ org.talend.designer.codegen.perlmodule/src/main/java/org/talend/designer/codegen/perlmodule/ui/views/ModulesView.java
@@ -21,31 +21,14 @@
 // ============================================================================
 package org.talend.designer.codegen.perlmodule.ui.views;
 
-import org.apache.log4j.Logger;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ActionHandler;
-import org.eclipse.ui.contexts.IContextActivation;
-import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
-import org.talend.commons.ui.swt.tableviewer.TableViewerCreator;
-import org.talend.commons.ui.swt.tableviewer.TableViewerCreatorColumn;
-import org.talend.commons.ui.swt.tableviewer.TableViewerCreator.LAYOUT_MODE;
-import org.talend.commons.ui.swt.tableviewer.TableViewerCreator.SORT;
-import org.talend.commons.utils.data.bean.IBeanPropertyAccessors;
-import org.talend.designer.codegen.perlmodule.ModuleNeeded;
-import org.talend.designer.codegen.perlmodule.model.ModulesNeededProvider;
+import org.talend.commons.exception.RuntimeExceptionHandler;
 import org.talend.designer.codegen.perlmodule.ui.actions.CheckModulesAction;
 
 /**
@@ -56,173 +39,53 @@ import org.talend.designer.codegen.perlmodule.ui.actions.CheckModulesAction;
  */
 public class ModulesView extends ViewPart {
 
-    private static Logger log = Logger.getLogger(ModulesView.class);
-
-    protected static final String ID_STATUS = "status";
-
-    private static TableViewerCreator tableViewerCreator;
-
     private CheckModulesAction checkAction;
 
-    public ModulesView() {
-    }
+    private Composite modulesViewComposite;
 
+    /**
+     * Ask the modules view composite to refresh its content.
+     * 
+     * yzhang Comment method "refresh".
+     */
     public void refresh() {
-        ModulesNeededProvider.check();
-        tableViewerCreator.getTableViewer().refresh();
+        if (this.modulesViewComposite instanceof IModulesViewComposite) {
+            IModulesViewComposite perlModulesViewComposite = (IModulesViewComposite) this.modulesViewComposite;
+            perlModulesViewComposite.refresh();
+        } else {
+            RuntimeExceptionHandler.process(new RuntimeException(
+                    "Compoiste in moudles view should be an instance of IModulesViewComposite"));
+        }
+
     }
 
     /*
-     * (non-Javadoc)
+     * Creat the content of this view.
      * 
      * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
      */
     @Override
     public void createPartControl(Composite parent) {
+        // If the content of this view need to display libries of Java, a new composite for it should be developed. And
+        // replace the PerlModulesViewComposite here.
 
-        parent.setLayout(new FormLayout());
-        FormData formData = new FormData();
-
-        Composite rightPartComposite = new Composite(parent, SWT.NONE);
-        formData = new FormData();
-        formData.top = new FormAttachment(0);
-        formData.left = new FormAttachment(0);
-        formData.right = new FormAttachment(100);
-        formData.bottom = new FormAttachment(100);
-        rightPartComposite.setLayoutData(formData);
-        rightPartComposite.setLayout(new FillLayout());
-
-        tableViewerCreator = new TableViewerCreator(rightPartComposite);
-        tableViewerCreator.setCheckboxInFirstColumn(false);
-        tableViewerCreator.setColumnsResizableByDefault(true);
-        tableViewerCreator.setLayoutMode(LAYOUT_MODE.FILL_HORIZONTAL);
-        tableViewerCreator.createTable();
-
-        TableViewerCreatorColumn column = new TableViewerCreatorColumn(tableViewerCreator);
-        column.setTitle("Status");
-        column.setId(ID_STATUS);
-        column.setSortable(true);
-        column.setImageProvider(new StatusImageProvider());
-        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<ModuleNeeded, String>() {
-
-            public String get(ModuleNeeded bean) {
-                String str = null;
-                switch (bean.getStatus()) {
-                case INSTALLED:
-                    str = "Installed";
-                    break;
-                case NOT_INSTALLED:
-                    str = "Not installed";
-                    break;
-                default:
-                    str = "Unknown";
-                }
-                return str;
-            }
-
-            public void set(ModuleNeeded bean, String value) {
-            }
-        });
-
-        column.setWeight(3);
-        column.setModifiable(false);
-
-        column = new TableViewerCreatorColumn(tableViewerCreator);
-        column.setTitle("Component");
-        column.setSortable(true);
-        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<ModuleNeeded, String>() {
-
-            public String get(ModuleNeeded bean) {
-                return bean.getComponentName();
-            }
-
-            public void set(ModuleNeeded bean, String value) {
-            }
-        });
-
-        column.setModifiable(false);
-        column.setWeight(3);
-
-        column = new TableViewerCreatorColumn(tableViewerCreator);
-        column.setTitle("Module");
-        column.setSortable(true);
-        tableViewerCreator.setDefaultSort(column, SORT.ASC);
-        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<ModuleNeeded, String>() {
-
-            public String get(ModuleNeeded bean) {
-                return bean.getModuleName();
-            }
-
-            public void set(ModuleNeeded bean, String value) {
-            }
-        });
-
-        column.setModifiable(false);
-        column.setWeight(3);
-
-        column = new TableViewerCreatorColumn(tableViewerCreator);
-        column.setTitle("Required for");
-
-        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<ModuleNeeded, String>() {
-
-            public String get(ModuleNeeded bean) {
-                return bean.getInformationMsg();
-            }
-
-            public void set(ModuleNeeded bean, String value) {
-            }
-        });
-
-        column.setModifiable(true);
-        column.setWeight(12);
-
-        column = new TableViewerCreatorColumn(tableViewerCreator);
-        column.setTitle("Required");
-        column.setImageProvider(new RequiredImageProvider());
-        column.setSortable(true);
-        column.setDisplayedValue("");
-        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<ModuleNeeded, String>() {
-
-            public String get(ModuleNeeded bean) {
-                return String.valueOf(bean.isRequired());
-            }
-
-            public void set(ModuleNeeded bean, String value) {
-            }
-        });
-
-        column.setModifiable(false);
-        column.setWeight(2);
-
-        tableViewerCreator.init(ModulesNeededProvider.getModulesNeeded());
-
+        modulesViewComposite = new PerlModulesViewComposite(parent);
         makeActions();
         contributeToActionBars();
-
-        FocusListener fl = new FocusListener() {
-
-            public void focusGained(FocusEvent e) {
-                log.trace("Modules gain focus");
-                IContextService contextService = (IContextService) PlatformUI.getWorkbench().getAdapter(IContextService.class);
-                ca = contextService.activateContext("talend.modules");
-            }
-
-            public void focusLost(FocusEvent e) {
-                log.trace("Modules lost focus");
-                if (ca != null) {
-                    IContextService contextService = (IContextService) PlatformUI.getWorkbench()
-                            .getAdapter(IContextService.class);
-                    contextService.deactivateContext(ca);
-                }
-            }
-        };
-
-        parent.addFocusListener(fl);
-        rightPartComposite.addFocusListener(fl);
-        tableViewerCreator.getTableViewer().getTable().addFocusListener(fl);
     }
 
-    private IContextActivation ca;
+    /*
+     * Set focus on the composite within this view.
+     * 
+     * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+     */
+    @Override
+    public void setFocus() {
+        if (this.modulesViewComposite == null) {
+            return;
+        }
+        this.modulesViewComposite.setFocus();
+    }
 
     private void makeActions() {
         checkAction = new CheckModulesAction(this);
@@ -240,16 +103,6 @@ public class ModulesView extends ViewPart {
 
     private void fillLocalToolBar(IToolBarManager manager) {
         manager.add(checkAction);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
-     */
-    @Override
-    public void setFocus() {
-        tableViewerCreator.getTableViewer().getTable().setFocus();
     }
 
 }
