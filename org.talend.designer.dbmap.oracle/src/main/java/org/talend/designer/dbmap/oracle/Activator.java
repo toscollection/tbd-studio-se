0e308e6ba54e1c823f216ba028348d87e81edc31
plegall <plegall@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-12 13:16:18 +0000

    Subversion administration: apply new header to all java class source files
    git-svn-id: http://talendforge.org/svn/tos/trunk@6924 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.dbmap.oracle/src/main/java/org/talend/designer/dbmap/oracle/Activator.java
+++ org.talend.designer.dbmap.oracle/src/main/java/org/talend/designer/dbmap/oracle/Activator.java
@@ -1,25 +1,15 @@
 // ============================================================================
 //
-// Talend Community Edition
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
 //
-// Copyright (C) 2006-2007 Talend - www.talend.com
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
+// You should have received a copy of the  agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//   
 // ============================================================================
-
 package org.talend.designer.dbmap.oracle;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
