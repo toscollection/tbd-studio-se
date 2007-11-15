644497961e7aa50dcb0933dee3f280a243f44afe
plegall <plegall@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-15 14:05:48 +0000

    Coding administration: refresh all org.talend.*/**/*.java headers with org.talend.developpement/normes/class_header.txt trunk r7037.
    git-svn-id: http://talendforge.org/svn/tos/trunk@7038 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.perl.editorwrapper/src/com/talend/perl/editorwrapper/views/PerlEditorSite.java
+++ org.talend.commons.perl.editorwrapper/src/com/talend/perl/editorwrapper/views/PerlEditorSite.java
@@ -8,7 +8,7 @@
 // You should have received a copy of the agreement
 // along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
-//   
+//
 // ============================================================================
 package com.talend.perl.editorwrapper.views;
 
