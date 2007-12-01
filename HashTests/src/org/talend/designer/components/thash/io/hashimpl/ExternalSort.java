aacffc2d1020b6755d82cdca9f167d3a6f06b6d5
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-12-01 23:27:31 +0000

    HashTests: Optimizations on ExternalSort
    git-svn-id: http://talendforge.org/svn/tos/trunk@7321 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- HashTests/src/org/talend/designer/components/thash/io/hashimpl/ExternalSort.java
+++ HashTests/src/org/talend/designer/components/thash/io/hashimpl/ExternalSort.java
@@ -9,13 +9,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
 import java.io.Serializable;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import org.talend.designer.components.thash.io.beans.Bean;
 import org.talend.designer.components.thash.io.beans.KeyForMap;
 
 /**
@@ -27,25 +30,53 @@ public class ExternalSort {
 
     public List<File> files = new ArrayList<File>();
 
-    public String workDirectory = "D:/temp/";
+    // public String workDirectory = "D:/temp/";
+    public String workDirectory = "/home/amaumont/hash_benchs/external_sort/";
 
     public int count = 0;
 
-    public void writeArray(List<Data> list) throws FileNotFoundException, IOException {
-        Collections.sort(list);
+    public void writeBuffer(Data[] list) throws FileNotFoundException, IOException {
+        long time1 = System.currentTimeMillis();
+        System.out.println("Sorting buffer...");
+        
+        
+        Arrays.sort(list);
+        
+        
+        long time2 = System.currentTimeMillis();
+        long deltaTimeSort = (time2 - time1);
+        int itemsPerSecSort = (int) ((float) list.length / (float) deltaTimeSort * 1000f);
+        System.out.println(deltaTimeSort + " milliseconds for " + list.length + " objects to sort in memory. " + itemsPerSecSort
+                + "  items/s ");
+
+        time1 = System.currentTimeMillis();
+        System.out.println("Writing ordered buffer in file...");
+
         File file = new File(workDirectory + "TEMP_" + count);
         count++;
         RandomAccessFile rw = new RandomAccessFile(file, "rw");
-        for (int i = 0; i < list.size(); i++) {
-            write(list.get(i), rw);
+        for (int i = 0; i < list.length; i++) {
+            write(list[i], rw);
         }
         rw.close();
         files.add(file);
+
+        
+        time2 = System.currentTimeMillis();
+        long deltaTimeWrite = (time2 - time1);
+        int itemsPerSecWrite = (int) ((float) list.length / (float) deltaTimeWrite * 1000f);
+        System.out.println(deltaTimeWrite + " milliseconds for " + list.length + " objects to write in file. " + itemsPerSecWrite
+                + "  items/s ");
+
+    
     }
 
     public Map<KeyForMap, KeyForMap> sort() throws IOException, ClassNotFoundException {
+
         while (files.size() > 1) {// for test, do not return THashMap
+
             List<File> inter = new ArrayList<File>();
+
             for (Iterator<File> itr = files.iterator(); itr.hasNext();) {
                 File one = itr.next();
                 if (itr.hasNext()) {
@@ -59,7 +90,7 @@ public class ExternalSort {
             }
             files = inter;
         }
-        //for test do not return the THashMap
+        // for test do not return the THashMap
 
         return null;
 
@@ -78,40 +109,47 @@ public class ExternalSort {
         RandomAccessFile rr1 = new RandomAccessFile(one, "r");
         RandomAccessFile rr2 = new RandomAccessFile(two, "r");
 
-        File output = new File(workDirectory + "TEMP_" + count);
+        long rr1Length = rr1.length();
+        long rr2Length = rr2.length();
         
+        DataContainer a = new DataContainer();
+        DataContainer b = new DataContainer();
+        
+        File output = new File(workDirectory + "TEMP_" + count);
+
         count++;
         RandomAccessFile rw = new RandomAccessFile(output, "rw");
-        Data a = (Data) read(rr1);
-        Data b = (Data) read(rr2);
+        
+        read(rr1, a);
+        read(rr2, b);
 
-        while (a != null && b != null) {
-            if (a.compareTo(b) < 0) {
-                write(a, rw);
-                if (rr1.getFilePointer() < rr1.length()) {
-                    a = (Data) read(rr1);
+        while (a.object != null && b.object != null) {
+            if (((Comparable)(a.object)).compareTo(b.object) < 0) {
+                write(a.data, rw);
+                if (a.cursorPosition < rr1Length) {
+                    read(rr1, a);
                 } else {
-                    a = null;
+                    a.reset();
                 }
             } else {
-                write(b, rw);
-                if (rr2.getFilePointer() < rr2.length()) {
-                    b = (Data) read(rr2);
+                write(b.data, rw);
+                if (b.cursorPosition < rr2Length) {
+                    read(rr2, b);
                 } else {
-                    b = null;
+                    b.reset();
                 }
             }
         }
 
-        if (a != null) {
-            write(a, rw);
+        if (a.data != null) {
+            write(a.data, rw);
         }
 
-        if (b != null) {
-            write(b, rw);
+        if (b.data != null) {
+            write(b.data, rw);
         }
 
-        if (rr1.getFilePointer() < rr1.length()) {
+        if (a.cursorPosition < rr1Length) {
             byte[] buffer = new byte[1024];
             int count = 0;
             while ((count = rr1.read(buffer)) > 0) {
@@ -119,7 +157,7 @@ public class ExternalSort {
             }
         }
 
-        if (rr2.getFilePointer() < rr2.length()) {
+        if (b.cursorPosition < rr2Length) {
             byte[] buffer = new byte[1024];
             int count = 0;
             while ((count = rr2.read(buffer)) > 0) {
@@ -154,14 +192,25 @@ public class ExternalSort {
             }
         }
 
-        int sizeBytes = byteArrayOutputStream.size();
-
-        w.writeInt(sizeBytes);
+        w.writeInt(byteArrayOutputStream.size());
         w.write(byteArrayOutputStream.toByteArray());
 
         byteArrayOutputStream.close();
 
     }
+
+    private void write(byte[] data, RandomAccessFile w) throws IOException {
+        w.writeInt(data.length);
+        w.write(data);
+    }
+    
+    private void read(RandomAccessFile r, DataContainer dataContainer) throws IOException, ClassNotFoundException {
+        dataContainer.data = new byte[r.readInt()];
+        r.read(dataContainer.data);
+        dataContainer.object = new ObjectInputStream(new ByteArrayInputStream(dataContainer.data)).readObject();
+        dataContainer.cursorPosition += 4 + dataContainer.data.length; 
+    }
+
 
     private Object read(RandomAccessFile r) throws IOException, ClassNotFoundException {
         byte[] byteArray = new byte[r.readInt()];
@@ -177,44 +226,91 @@ public class ExternalSort {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ExternalSort esort = new ExternalSort();
 
-        int nbItems = 1000000;
-
-        int bufferSize = 100000;
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        
+//        int nbItems = 60000000;
+//        int bufferSize = 2000000;
+        int nbItems = 60000000;
+        int bufferSize = 10000000;
+//        int nbItems = 1000000;
+//        int bufferSize = 100000;
+//         int nbItems = 20;
+//         int bufferSize = 2;
 
         Random rand = new Random(System.currentTimeMillis());
 
         long start = System.currentTimeMillis();
 
-        long lastTime = System.currentTimeMillis();
+        Data[] arrayData = new Data[bufferSize];
 
-        List<Data> list = new ArrayList<Data>(bufferSize);
+        int nbItemsProcessed = 0;
 
-        for (int i = 0; i < nbItems;) {
+        for (int i = 0; nbItemsProcessed < nbItems; i++) {
             int v = rand.nextInt(nbItems);
 
-            list.add(new Data("test" + v, v, 0));
+            arrayData[i] = new Data("test" + v, v, 0);
 
-            i++;
-            if (i % bufferSize == 0) {
-                esort.writeArray(list);
-                list = new ArrayList<Data>(bufferSize);
+            if (i == bufferSize - 1) {
+
+                esort.writeBuffer(arrayData);
+               
+                
+                long time1 = System.currentTimeMillis();
+
+                
+                Arrays.fill(arrayData, null);
+
+                
+                long time2 = System.currentTimeMillis();
+                long deltaTimeNull = (time2 - time1);
+                System.out.println(deltaTimeNull + " milliseconds for " + bufferSize + " objects to set buffer as null. ");
+                
+                
+                nbItemsProcessed += i + 1;
+                System.out.println(numberFormat.format(nbItemsProcessed) + " / " + numberFormat.format(nbItems) + " processed.");
+                i = -1;
             }
+
         }
-        if (list.size() > 0) {
-            esort.writeArray(list);
-        }
+        System.out.println("Final process : merging file...");
+        long time1 = System.currentTimeMillis();
+        
+        
         esort.sort();
 
+        
+        long time2 = System.currentTimeMillis();
+        long deltaTimeMerge = (time2 - time1);
+        int itemsPerSecMerge = (int) ((float) nbItems / (float) deltaTimeMerge * 1000f);
+        System.out.println(deltaTimeMerge + " milliseconds for " + nbItems + " ordered objects to merge. " + itemsPerSecMerge
+                + "  items/s ");
+
+        
         long end = System.currentTimeMillis();
 
         long deltaTime = (end - start);
 
         int itemsPerSec = (int) ((float) nbItems / (float) deltaTime * 1000f);
 
-        System.out.println(deltaTime + " milliseconds for " + nbItems + " objects to WRITE. " + itemsPerSec + "  items/s ");
+        System.out.println(deltaTime + " milliseconds for " + nbItems + " objects all sort process. " + itemsPerSec + "  items/s ");
 
     }
 
+}
+
+class DataContainer {
+    
+    long cursorPosition;
+    
+    Object object;
+    
+    byte[] data;
+
+    
+    public void reset() {
+        object = null;
+        data = null;
+    }
 }
 
 /**
@@ -259,7 +355,6 @@ class Data implements Serializable, Comparable<Data> {
         return "name=" + name + ", " + "age=" + id;
     }
 
-    @Override
     public int compareTo(Data o) {
         return this.id - o.id;
     }
