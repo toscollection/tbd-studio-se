473821e7bdf8116bf143f02beb83125233e947c3
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-03-23 10:54:30 +0000

    feature 6463 modified: branding modification.
    git-svn-id: http://talendforge.org/svn/tos/trunk@22960 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp.branding.thales/src/main/java/org/talend/rcp/branding/thales/ThalesActionBarHelper.java
+++ org.talend.rcp.branding.thales/src/main/java/org/talend/rcp/branding/thales/ThalesActionBarHelper.java
@@ -17,17 +17,32 @@ import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.preference.IPreferenceNode;
 import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.ui.internal.registry.ActionSetRegistry;
+import org.eclipse.ui.internal.registry.IActionSetDescriptor;
 import org.eclipse.ui.internal.registry.PerspectiveDescriptor;
 import org.eclipse.ui.internal.registry.PerspectiveRegistry;
 import org.eclipse.ui.internal.registry.ViewDescriptor;
 import org.eclipse.ui.internal.registry.ViewRegistry;
 import org.eclipse.ui.views.IViewDescriptor;
 import org.eclipse.update.internal.core.UpdateCore;
+import org.talend.core.i18n.Messages;
+import org.talend.core.ui.perspective.PerspectiveMenuManager;
 import org.talend.rcp.intro.ActionBarBuildHelper;
+import org.talend.rcp.intro.ExportCommandAction;
+import org.talend.rcp.intro.SwitchProjectAction;
+import org.talend.repository.model.ProxyRepositoryFactory;
+import org.talend.repository.ui.actions.toolbar.ProjectSettingsAction;
 
 /**
  * DOC aiming class global comment. Detailled comment
@@ -41,7 +56,112 @@ public class ThalesActionBarHelper extends ActionBarBuildHelper {
      */
     @Override
     public void fillMenuBar(IMenuManager menuBar) {
-        super.fillMenuBar(menuBar);
+        ActionSetRegistry reg = WorkbenchPlugin.getDefault().getActionSetRegistry();
+        IActionSetDescriptor[] actionSets = reg.getActionSets();
+
+        List<String> list = new ArrayList<String>();
+        for (String item : ACTIONSETID) {
+            list.add(item);
+        }
+        if (ProxyRepositoryFactory.getInstance().isUserReadOnlyOnCurrentProject()) {
+            list.add("org.talend.repository.CreateactionSet"); //$NON-NLS-1$
+        }
+        for (int i = 0; i < actionSets.length; i++) {
+            if (list.contains(actionSets[i].getId())) {
+                removeAction(reg, actionSets[i]);
+            }
+        }
+
+        fileMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.menuFileLabel"), IWorkbenchActionConstants.M_FILE); //$NON-NLS-1$
+        menuBar.add(fileMenu);
+        // MenuManager subFile = new MenuManager("&New", IWorkbenchActionConstants.NEW_EXT);
+        // subFile.add(ActionFactory.NEW.create(window));
+        // fileMenu.add(subFile);
+
+        IWorkbenchAction closeAction = ActionFactory.CLOSE.create(window);
+        fileMenu.add(closeAction);
+        actionBarConfigurer.registerGlobalAction(closeAction);
+
+        IWorkbenchAction closeAllAction = ActionFactory.CLOSE_ALL.create(window);
+        fileMenu.add(closeAllAction);
+        actionBarConfigurer.registerGlobalAction(closeAllAction);
+        fileMenu.add(new Separator());
+
+        fileMenu.add(ActionFactory.SAVE.create(window));
+
+        IWorkbenchAction saveAllAction = ActionFactory.SAVE_ALL.create(window);
+        fileMenu.add(saveAllAction);
+        actionBarConfigurer.registerGlobalAction(saveAllAction);
+        // fileMenu.add(ActionFactory.SAVE_AS.create(window));
+        fileMenu.add(new Separator());
+        fileMenu.add(ActionFactory.PRINT.create(window));
+        fileMenu.add(new Separator());
+        switchProjectAction = new SwitchProjectAction();
+        fileMenu.add(switchProjectAction);
+        ProjectSettingsAction projSetting = new ProjectSettingsAction();
+        fileMenu.add(projSetting);
+        fileMenu.add(new Separator());
+
+        fileMenu.add(ActionFactory.IMPORT.create(window));
+        fileMenu.add(new ExportCommandAction(window));
+
+        fileMenu.add(new Separator());
+        fileMenu.add(ActionFactory.QUIT.create(window));
+        editMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.menuEditLabel"), IWorkbenchActionConstants.M_EDIT); //$NON-NLS-1$
+        menuBar.add(editMenu);
+        editMenu.add(new Separator(GROUP_UNDO));
+        editMenu.add(new Separator(GROUP_COPY));
+        editMenu.add(new Separator(GROUP_DELETE));
+        editMenu.appendToGroup(GROUP_UNDO, ActionFactory.UNDO.create(window));
+        editMenu.appendToGroup(GROUP_UNDO, ActionFactory.REDO.create(window));
+        editMenu.appendToGroup(GROUP_COPY, ActionFactory.CUT.create(window));
+        editMenu.appendToGroup(GROUP_COPY, ActionFactory.COPY.create(window));
+        editMenu.appendToGroup(GROUP_COPY, ActionFactory.PASTE.create(window));
+        editMenu.appendToGroup(GROUP_DELETE, ActionFactory.DELETE.create(window));
+        editMenu.appendToGroup(GROUP_DELETE, ActionFactory.SELECT_ALL.create(window));
+        editMenu.add(new GroupMarker(IWorkbenchActionConstants.FIND_EXT));
+
+        MenuManager navigateMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.navigateLabel"), IWorkbenchActionConstants.M_NAVIGATE); //$NON-NLS-1$
+        navigateMenu.add(new GroupMarker(IWorkbenchActionConstants.NAV_START));
+        navigateMenu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+        navigateMenu.add(new GroupMarker(IWorkbenchActionConstants.SHOW_EXT));
+        // see bug 0005492: Could not read the editor (XML Editor)
+        navigateMenu.add(new GroupMarker(IWorkbenchActionConstants.OPEN_EXT));
+
+        MenuManager gotoMenu = new MenuManager(Messages.getString("ApplicationActionBarAdvisor.gotoLabel"), //$NON-NLS-1$
+                IWorkbenchActionConstants.GO_TO);
+        navigateMenu.add(gotoMenu);
+
+        menuBar.add(navigateMenu);
+
+        MenuManager projectMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.projectLabel"), IWorkbenchActionConstants.M_PROJECT); //$NON-NLS-1$
+        menuBar.add(projectMenu);
+
+        MenuManager perspMenu = new PerspectiveMenuManager();
+
+        windowMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.menuWindowLabel"), IWorkbenchActionConstants.M_WINDOW); //$NON-NLS-1$
+        menuBar.add(windowMenu);
+        windowMenu.add(perspMenu);
+        windowMenu.add(new Separator());
+        windowMenu.add(ActionFactory.MAXIMIZE.create(window));
+
+        windowMenu.add(ActionFactory.PREFERENCES.create(window));
+
+        helpMenu = new MenuManager(
+                Messages.getString("ApplicationActionBarAdvisor.menuHelpLabel"), IWorkbenchActionConstants.M_HELP); //$NON-NLS-1$
+        menuBar.add(helpMenu);
+
+        introAction = ActionFactory.INTRO.create(window);
+        // Help
+        helpMenu.add(introAction);
+        helpMenu.add(ActionFactory.HELP_CONTENTS.create(window));
+        IWorkbenchAction create = ActionFactory.ABOUT.create(window);
+        helpMenu.add(create);
     }
 
     /*
