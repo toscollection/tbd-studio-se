7171d2d4069f452dc7f96b7cafb31d74abcfe41e
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-12 06:30:13 +0000

    feature TDI-19914: add missing class...
    git-svn-id: http://talendforge.org/svn/tos/trunk@81550 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.oozie.scheduler/src/org/talend/oozie/scheduler/views/OozieJobTrackerListener.java
@@ -0,0 +1,60 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.oozie.scheduler.views;
+
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.talend.core.model.process.IProcess2;
+import org.talend.designer.core.ui.IJobTrackerListener;
+
+/**
+ * DOC nrousseau class global comment. Detailled comment
+ */
+public class OozieJobTrackerListener implements IJobTrackerListener {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.ui.IJobTrackerListener#focusOnJob(org.talend.core.model.process.IProcess2)
+     */
+    @Override
+    public void focusOnJob(IProcess2 process) {
+        IWorkbenchWindow ww = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (ww != null && ww.getActivePage() != null) {
+            IViewPart viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+                    .findView("org.talend.oozie.scheduler.views.OozieSchedulerView");
+            if (viewPart != null) {
+                ((OozieSchedulerView) viewPart).refresh();
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.ui.IJobTrackerListener#allJobClosed()
+     */
+    @Override
+    public void allJobClosed() {
+        IWorkbenchWindow ww = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (ww != null && ww.getActivePage() != null) {
+            IViewPart viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+                    .findView("org.talend.oozie.scheduler.views.OozieSchedulerView");
+            if (viewPart != null) {
+                ((OozieSchedulerView) viewPart).refresh();
+            }
+        }
+    }
+
+}
