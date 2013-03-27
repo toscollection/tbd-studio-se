9cdf7541bfb1339139e5547bd5a57972447b3bc4
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-27 10:08:51 +0000

    feature TDI-25172 : hide hbase action for MapR 1.2.8
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@100299 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.hadoopcluster/src/org/talend/repository/hbase/action/CreateHBaseAction.java
+++ org.talend.repository.hadoopcluster/src/org/talend/repository/hbase/action/CreateHBaseAction.java
@@ -5,7 +5,11 @@ import java.util.Map;
 import org.talend.core.database.EDatabaseTypeName;
 import org.talend.core.database.conn.ConnParameterKeys;
 import org.talend.core.database.conn.template.EDatabaseConnTemplate;
+import org.talend.core.hadoop.version.EHadoopVersion4Drivers;
 import org.talend.repository.hadoopcluster.action.common.CreateHadoopDBNodeAction;
+import org.talend.repository.hadoopcluster.util.HCRepositoryUtil;
+import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.model.hadoopcluster.HadoopClusterConnection;
 import org.talend.repository.model.hadoopcluster.HadoopClusterConnectionItem;
 
 /**
@@ -31,6 +35,20 @@ public class CreateHBaseAction extends CreateHadoopDBNodeAction {
         initMap.put(ConnParameterKeys.CONN_PARA_KEY_DB_TYPE, EDatabaseConnTemplate.HBASE.getDBTypeName());
         initMap.put(ConnParameterKeys.CONN_PARA_KEY_DB_PRODUCT, EDatabaseTypeName.HBASE.getProduct());
         initMap.put(ConnParameterKeys.CONN_PARA_KEY_DB_PORT, EDatabaseConnTemplate.HBASE.getDefaultPort());
+    }
+
+    @Override
+    protected boolean hideAction(RepositoryNode node) {
+        HadoopClusterConnectionItem hcConnectionItem = HCRepositoryUtil.getHCConnectionItemFromRepositoryNode(node);
+        if (hcConnectionItem != null) {
+            HadoopClusterConnection hcConnection = (HadoopClusterConnection) hcConnectionItem.getConnection();
+            EHadoopVersion4Drivers version4Drivers = EHadoopVersion4Drivers.indexOfByVersion(hcConnection.getDfVersion());
+            if (EHadoopVersion4Drivers.MAPR_EMR.equals(version4Drivers)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
