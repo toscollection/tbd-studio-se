473b2f0b9f1509447ea0d5b08fc7c8a2c23f1dda
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-10 07:10:13 +0000

    bug TDI-25363 fixed : Change GUI part
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@100962 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.hdfs/src/org/talend/repository/hdfs/action/CreateHDFSAction.java
+++ org.talend.repository.hdfs/src/org/talend/repository/hdfs/action/CreateHDFSAction.java
@@ -3,12 +3,16 @@ package org.talend.repository.hdfs.action;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.ui.IWorkbench;
 import org.talend.commons.ui.runtime.image.IImage;
+import org.talend.core.hadoop.version.EHadoopVersion4Drivers;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.repository.hadoopcluster.action.common.CreateHadoopNodeAction;
+import org.talend.repository.hadoopcluster.util.HCRepositoryUtil;
 import org.talend.repository.hdfs.node.model.HDFSRepositoryNodeType;
 import org.talend.repository.hdfs.ui.HDFSWizard;
 import org.talend.repository.hdfs.util.EHDFSImage;
 import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.model.hadoopcluster.HadoopClusterConnection;
+import org.talend.repository.model.hadoopcluster.HadoopClusterConnectionItem;
 import org.talend.repository.model.hdfs.HDFSConnectionItem;
 
 /**
@@ -34,6 +38,20 @@ public class CreateHDFSAction extends CreateHadoopNodeAction {
     @Override
     public Class getClassForDoubleClick() {
         return HDFSConnectionItem.class;
+    }
+
+    @Override
+    protected boolean hideAction(RepositoryNode node) {
+        HadoopClusterConnectionItem hcConnectionItem = HCRepositoryUtil.getHCConnectionItemFromRepositoryNode(node);
+        if (hcConnectionItem != null) {
+            HadoopClusterConnection hcConnection = (HadoopClusterConnection) hcConnectionItem.getConnection();
+            EHadoopVersion4Drivers version4Drivers = EHadoopVersion4Drivers.indexOfByVersion(hcConnection.getDfVersion());
+            if (EHadoopVersion4Drivers.APACHE_0_20_203.equals(version4Drivers)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
