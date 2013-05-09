7995fa2fa7936bd24c42eb1c6cffc777ae5deb8f
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-09 02:15:52 +0000

    bug TDI-25877 fixed : HCatalogSchemaWizard show two GUI part
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@102486 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.hcatalog/src/org/talend/repository/hcatalog/ui/HCatalogSchemaForm.java
+++ org.talend.repository.hcatalog/src/org/talend/repository/hcatalog/ui/HCatalogSchemaForm.java
@@ -127,7 +127,6 @@ public class HCatalogSchemaForm extends AbstractHCatalogForm {
                 break;
             }
         }
-        setupForm();
     }
 
     public void initializeForm() {
