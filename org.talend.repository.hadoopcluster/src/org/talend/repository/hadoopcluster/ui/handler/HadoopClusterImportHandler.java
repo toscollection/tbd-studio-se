3131c11f3ba0c4d35d65f2e287393991b8c6fed5
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-26 08:58:49 +0000

    TUP-979: enable import hadoop clusters.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@107088 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/ui/handler/HadoopClusterImportHandler.java
@@ -0,0 +1,72 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.hadoopcluster.ui.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.talend.core.GlobalServiceRegister;
+import org.talend.core.hadoop.IHadoopClusterService;
+import org.talend.core.model.properties.Item;
+import org.talend.repository.items.importexport.handlers.imports.AbstractImportHandler;
+import org.talend.repository.items.importexport.ui.wizard.imports.managers.ResourcesManager;
+import org.talend.repository.items.importexport.ui.wizard.imports.models.ItemRecord;
+
+/**
+ * DOC ggu class global comment. Detailled comment
+ */
+public class HadoopClusterImportHandler extends AbstractImportHandler {
+
+    public HadoopClusterImportHandler() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.AbstractImportHandler#findRelatedItemRecord(org.eclipse
+     * .core.runtime.IProgressMonitor,
+     * org.talend.repository.items.importexport.ui.wizard.imports.managers.ResourcesManager,
+     * org.talend.repository.items.importexport.ui.wizard.imports.models.ItemRecord,
+     * org.talend.repository.items.importexport.ui.wizard.imports.models.ItemRecord[])
+     */
+    @Override
+    public List<ItemRecord> findRelatedItemRecord(IProgressMonitor monitor, ResourcesManager resManager,
+            ItemRecord selectedItemRecord, ItemRecord[] allImportItemRecords) {
+
+        List<ItemRecord> relatedItemRecords = new ArrayList<ItemRecord>();
+        relatedItemRecords.addAll(super.findRelatedItemRecord(monitor, resManager, selectedItemRecord, allImportItemRecords));
+
+        if (GlobalServiceRegister.getDefault().isServiceRegistered(IHadoopClusterService.class)) {
+            IHadoopClusterService hadoopClusterService = (IHadoopClusterService) GlobalServiceRegister.getDefault().getService(
+                    IHadoopClusterService.class);
+            final Item item = selectedItemRecord.getItem();
+            if (hadoopClusterService != null && hadoopClusterService.isHadoopClusterItem(item)) {
+                resolveItem(resManager, selectedItemRecord);
+
+                List<String> subitemIds = hadoopClusterService.getSubitemIdsOfHadoopCluster(item);
+                if (!subitemIds.isEmpty() && allImportItemRecords != null) {
+                    for (ItemRecord ir : allImportItemRecords) {
+                        if (ir.getProperty() != null && subitemIds.contains(ir.getProperty().getId())) {
+                            relatedItemRecords.add(ir);
+                        }
+                    }
+                }
+            }
+        }
+        return relatedItemRecords;
+    }
+
+}
---
+++ org.talend.repository.hdfs/src/org/talend/repository/hdfs/ui/handler/HdfsImportHandler.java
@@ -0,0 +1,55 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.hdfs.ui.handler;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.talend.repository.items.importexport.handlers.imports.MetadataConnectionImportHandler;
+import org.talend.repository.items.importexport.ui.wizard.imports.managers.ResourcesManager;
+import org.talend.repository.items.importexport.ui.wizard.imports.models.ItemRecord;
+
+/**
+ * DOC ggu class global comment. Detailled comment
+ */
+public class HdfsImportHandler extends MetadataConnectionImportHandler {
+
+    /**
+     * DOC ggu HdfsImportHandler constructor comment.
+     */
+    public HdfsImportHandler() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.ImportHandler#calcItemRecord(org.eclipse.core.runtime
+     * .IProgressMonitor, org.talend.repository.items.importexport.ui.wizard.imports.managers.ResourcesManager,
+     * org.eclipse.core.runtime.IPath, boolean, java.util.List)
+     */
+    @Override
+    public ItemRecord calcItemRecord(IProgressMonitor progressMonitor, ResourcesManager resManager, IPath resourcePath,
+            boolean overwrite, List<ItemRecord> existeditems) {
+        final ItemRecord itemRecord = super.calcItemRecord(progressMonitor, resManager, resourcePath, overwrite, existeditems);
+        if (itemRecord != null) {
+            // won't display in import dialog, will be implict to import by hadoop cluster(method
+            // findRelatedItemRecord).
+            itemRecord.setVisible(false);
+        }
+        return itemRecord;
+    }
+
+}
