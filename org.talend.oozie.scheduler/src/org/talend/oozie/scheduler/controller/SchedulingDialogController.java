774efd502c4f279d9377aa20e70aa5835f9ffdd3
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-26 07:38:38 +0000

    bug TDI-26329 fixed: oozie schedule: if config the time before now it can complete
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@115522 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.oozie.scheduler/src/org/talend/oozie/scheduler/controller/SchedulingDialogController.java
+++ org.talend.oozie.scheduler/src/org/talend/oozie/scheduler/controller/SchedulingDialogController.java
@@ -29,7 +29,7 @@ import org.talend.oozie.scheduler.utils.TOozieDateUtils;
  */
 public class SchedulingDialogController {
 
-    private TOozieSchedulerDialog schedulingDialog;
+    private final TOozieSchedulerDialog schedulingDialog;
 
     public static final String PATTERN = "[1-9][0-9]*";
 
@@ -59,14 +59,24 @@ public class SchedulingDialogController {
 
     public boolean checkIfAllValidOrNot() {
         boolean okBtnIsEnabled = false;
-        boolean startTimeHasValue = hasStartTimeValue();
-        boolean endTimeHasValue = hasEndTimeValue();
+        boolean checkTimeValue = checkTime();
         boolean frequencyValueIsValid = checkFrequencyValue();
-        if (startTimeHasValue && endTimeHasValue && frequencyValueIsValid)
+        if (checkTimeValue && frequencyValueIsValid)
             okBtnIsEnabled = true;
         else
             okBtnIsEnabled = false;
         return okBtnIsEnabled;
+    }
+
+    private boolean checkTime() {
+        if (hasStartTimeValue() && hasEndTimeValue()) {
+            Date startTimeValue = schedulingDialog.getStartDate();
+            Date endTimeValue = schedulingDialog.getEndDate();
+            Date now = new Date();
+            boolean b = now.getTime() - startTimeValue.getTime() < 10000;
+            return b && endTimeValue.after(startTimeValue);
+        }
+        return false;
     }
 
     protected boolean hasEndTimeValue() {
