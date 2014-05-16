21fd02851c407e10991c2c5dd1a18d62ef478cb3
cmeng <cmeng@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-05-16 09:04:14 +0000

    feature TUP-1628 : remove all internal use of Eclipse APIs for DI plugins
    git-svn-id: http://talendforge.org/svn/tos/trunk@117578 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp.branding.tos.bigdata/src/main/java/org/talend/rcp/branding/tos/bigdata/HortonActionBarHelper.java
+++ org.talend.rcp.branding.tos.bigdata/src/main/java/org/talend/rcp/branding/tos/bigdata/HortonActionBarHelper.java
@@ -17,12 +17,6 @@ import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.jface.preference.IPreferenceNode;
-import org.eclipse.ui.IPerspectiveDescriptor;
-import org.eclipse.ui.internal.registry.PerspectiveDescriptor;
-import org.eclipse.ui.internal.registry.PerspectiveRegistry;
-import org.eclipse.ui.internal.registry.ViewDescriptor;
-import org.eclipse.ui.internal.registry.ViewRegistry;
-import org.eclipse.ui.views.IViewDescriptor;
 import org.talend.rcp.intro.ActionBarBuildHelper;
 
 /**
@@ -44,48 +38,50 @@ public class HortonActionBarHelper extends ActionBarBuildHelper {
             helpMenu.remove(id);
         }
 
-        String[] perspectivesId = { "org.eclipse.jdt.ui.JavaPerspective", "org.eclipse.team.ui.TeamSynchronizingPerspective",
-                "org.eclipse.ui.resourcePerspective", "org.eclipse.pde.ui.PDEPerspective" };
+        // String[] perspectivesId = { "org.eclipse.jdt.ui.JavaPerspective",
+        // "org.eclipse.team.ui.TeamSynchronizingPerspective",
+        // "org.eclipse.ui.resourcePerspective", "org.eclipse.pde.ui.PDEPerspective" };
+        //
+        // List<IPerspectiveDescriptor> perspectivesToDelete = new ArrayList<IPerspectiveDescriptor>();
+        //
+        // for (IPerspectiveDescriptor desc : window.getWorkbench().getPerspectiveRegistry().getPerspectives()) {
+        // if (ArrayUtils.contains(perspectivesId, desc.getId())) {
+        // perspectivesToDelete.add(desc);
+        // }
+        // }
+        //
+        // for (IPerspectiveDescriptor desc : perspectivesToDelete) {
+        // PerspectiveDescriptor perspDesc = (PerspectiveDescriptor) desc;
+        // PerspectiveRegistry registry = (PerspectiveRegistry) window.getWorkbench().getPerspectiveRegistry();
+        // PerspectiveDescriptor[] descriptors = { perspDesc };
+        // registry.removeExtension(perspDesc.getConfigElement().getDeclaringExtension(), descriptors);
+        // }
 
-        List<IPerspectiveDescriptor> perspectivesToDelete = new ArrayList<IPerspectiveDescriptor>();
-
-        for (IPerspectiveDescriptor desc : window.getWorkbench().getPerspectiveRegistry().getPerspectives()) {
-            if (ArrayUtils.contains(perspectivesId, desc.getId())) {
-                perspectivesToDelete.add(desc);
-            }
-        }
-
-        for (IPerspectiveDescriptor desc : perspectivesToDelete) {
-            PerspectiveDescriptor perspDesc = (PerspectiveDescriptor) desc;
-            PerspectiveRegistry registry = (PerspectiveRegistry) window.getWorkbench().getPerspectiveRegistry();
-            PerspectiveDescriptor[] descriptors = { perspDesc };
-            registry.removeExtension(perspDesc.getConfigElement().getDeclaringExtension(), descriptors);
-        }
-
-        String[] viewsId = { "org.eclipse.ant.ui.views.AntView", "org.eclipse.pde.runtime.RegistryBrowser",
-                "org.eclipse.pde.ui.DependenciesView", "org.eclipse.pde.ui.PluginsView", "org.eclipse.search.SearchResultView",
-                "org.eclipse.search.ui.views.SearchView", "org.eclipse.team.sync.views.SynchronizeView",
-                "org.eclipse.team.ui.GenericHistoryView", "org.eclipse.ui.browser.view",
-                "org.eclipse.ui.cheatsheets.views.CheatSheetView", "org.eclipse.ui.texteditor.TemplatesView",
-                "org.eclipse.ui.views.AllMarkersView", "org.eclipse.ui.views.BookmarkView",
-                "org.eclipse.team.sync.views.SynchronizeView", "org.eclipse.ui.views.ProblemView",
-                "org.eclipse.ui.views.PropertySheet", "org.eclipse.ui.views.TaskList",
-                "org.eclipse.ui.navigator.ProjectExplorer", "org.eclipse.wst.common.snippets.internal.ui.SnippetsView" };
-
-        List<IViewDescriptor> viewsToDelete = new ArrayList<IViewDescriptor>();
-
-        for (IViewDescriptor desc : window.getWorkbench().getViewRegistry().getViews()) {
-            if (ArrayUtils.contains(viewsId, desc.getId())) {
-                viewsToDelete.add(desc);
-            }
-        }
-
-        for (IViewDescriptor desc : viewsToDelete) {
-            ViewDescriptor viewDesc = (ViewDescriptor) desc;
-            ViewRegistry registry = (ViewRegistry) window.getWorkbench().getViewRegistry();
-            ViewDescriptor[] descriptors = { viewDesc };
-            registry.removeExtension(viewDesc.getConfigurationElement().getDeclaringExtension(), descriptors);
-        }
+        // String[] viewsId = { "org.eclipse.ant.ui.views.AntView", "org.eclipse.pde.runtime.RegistryBrowser",
+        // "org.eclipse.pde.ui.DependenciesView", "org.eclipse.pde.ui.PluginsView",
+        // "org.eclipse.search.SearchResultView",
+        // "org.eclipse.search.ui.views.SearchView", "org.eclipse.team.sync.views.SynchronizeView",
+        // "org.eclipse.team.ui.GenericHistoryView", "org.eclipse.ui.browser.view",
+        // "org.eclipse.ui.cheatsheets.views.CheatSheetView", "org.eclipse.ui.texteditor.TemplatesView",
+        // "org.eclipse.ui.views.AllMarkersView", "org.eclipse.ui.views.BookmarkView",
+        // "org.eclipse.team.sync.views.SynchronizeView", "org.eclipse.ui.views.ProblemView",
+        // "org.eclipse.ui.views.PropertySheet", "org.eclipse.ui.views.TaskList",
+        // "org.eclipse.ui.navigator.ProjectExplorer", "org.eclipse.wst.common.snippets.internal.ui.SnippetsView" };
+        //
+        // List<IViewDescriptor> viewsToDelete = new ArrayList<IViewDescriptor>();
+        //
+        // for (IViewDescriptor desc : window.getWorkbench().getViewRegistry().getViews()) {
+        // if (ArrayUtils.contains(viewsId, desc.getId())) {
+        // viewsToDelete.add(desc);
+        // }
+        // }
+        //
+        // for (IViewDescriptor desc : viewsToDelete) {
+        // ViewDescriptor viewDesc = (ViewDescriptor) desc;
+        // ViewRegistry registry = (ViewRegistry) window.getWorkbench().getViewRegistry();
+        // ViewDescriptor[] descriptors = { viewDesc };
+        // registry.removeExtension(viewDesc.getConfigurationElement().getDeclaringExtension(), descriptors);
+        // }
 
         String[] prefsId = { "org.eclipse.ant.ui.AntPreferencePage",
                 "org.eclipse.datatools.connectivity.internal.ui.preferences.DataToolsMainPage",
