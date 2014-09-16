abb4db13d6cb6317b9305a0abc068731976198f5
ycbai <ycbai@talend.com>
2014-09-16 09:44:24 +0800

    TBD-949 : Refactor hadoop cluster form

---
+++ main/plugins/org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/ui/common/IHadoopClusterInfoForm.java
@@ -0,0 +1,33 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.hadoopcluster.ui.common;
+
+import org.talend.repository.ui.swt.utils.AbstractForm.ICheckListener;
+
+/**
+ * created by ycbai on 2014年9月15日 Detailled comment
+ *
+ */
+public interface IHadoopClusterInfoForm {
+
+    public void updateForm();
+
+    public void setListener(ICheckListener listener);
+
+    public void setReadOnly(boolean readOnly);
+
+    public boolean checkFieldsValue();
+
+    public void dispose();
+
+}
