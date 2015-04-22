5919cbbb933cd054fd65a46d4637dc70747c9054
ycbai <ycbai@talend.com>
2015-04-22 16:49:45 +0800

    [TBD-1815] Oozie job cannot run successfully when using HDP 2.1

---
+++ main/plugins/org.talend.oozie.scheduler/src/org/talend/oozie/scheduler/constants/IOoziePrefConstants.java
@@ -0,0 +1,25 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.oozie.scheduler.constants;
+
+/**
+ * created by ycbai on 2015年4月22日 Detailled comment
+ *
+ */
+public interface IOoziePrefConstants {
+
+    public static final String OOZIE_STATUS_TIMEOUT = "OOZIE_STATUS_TIMEOUT"; //$NON-NLS-1$
+
+    public static final String OOZIE_STATUS_TIMEOUT_DEFAULT = "-1"; //$NON-NLS-1$
+
+}
