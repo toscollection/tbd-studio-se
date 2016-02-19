b3b7a2a9d66943749c79cca2d2b14eac0620859e
pbailly <pbailly@talend.com>
2016-02-19 18:19:40 +0100

    [TbD-3147] Add jar for S3 on HDP 2.2

---
+++ test/plugins/org.talend.hadoop.distribution.hdp220.test/src/org/talend/hadoop/distribution/hdp220/test/modulegroup/HDP220PigOutputNodeModuleGroupTest.java
@@ -0,0 +1,56 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.hdp220.test.modulegroup;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.hdp220.HDP220Constant;
+import org.talend.hadoop.distribution.hdp220.modulegroup.HDP220PigOutputNodeModuleGroup;
+
+/**
+ * created by pbailly on 16 Feb 2016 Detailled comment
+ *
+ */
+public class HDP220PigOutputNodeModuleGroupTest {
+
+    @Test
+    public void testModuleGroups() throws Exception {
+        Map<String, String> results = new HashMap<String, String>();
+
+        results.put(HDP220Constant.SPARK_S3_MRREQUIRED_MODULE_GROUP.getModuleName(),
+                "(#LINK@NODE.ASSOCIATED_PIG_LOAD.DISTRIBUTION=='HORTONWORKS') AND (#LINK@NODE.ASSOCIATED_PIG_LOAD.PIG_VERSION=='HDP_2_2') "
+                        + "AND (S3_LOCATION=='true') AND (STORE!='HCATSTORER') AND (STORE!='HBASESTORAGE')");
+
+        Set<DistributionModuleGroup> moduleGroups = HDP220PigOutputNodeModuleGroup.getModuleGroups();
+        assertEquals(results.size(), moduleGroups.size());
+        moduleGroups.iterator();
+        for (DistributionModuleGroup module : moduleGroups) {
+            assertTrue("Should contain module " + module.getModuleName(), results.containsKey(module.getModuleName()));
+            if (results.get(module.getModuleName()) == null) {
+                assertTrue("The condition of the module " + module.getModuleName() + " is not null.",
+                        results.get(module.getModuleName()) == null);
+            } else {
+                assertTrue(
+                        "The condition of the module " + module.getModuleName() + " is null, but it should be "
+                                + results.get(module.getModuleName()) + ".", results.get(module.getModuleName()) != null);
+                assertEquals(results.get(module.getModuleName()), module.getRequiredIf().getConditionString());
+            }
+        }
+    }
+}
