58992d2fa318dab0dcfdfb88b4d44bfd83448649
pbailly <pbailly@talend.com>
2016-04-11 10:58:35 +0200

    [TBD-3016] Add JUNIT tests.

---
+++ test/plugins/org.talend.hadoop.distribution.mapr510.test/src/org/talend/hadoop/distribution/mapr510/test/MapR510DistributionTest.java
@@ -0,0 +1,94 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.mapr510.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.talend.hadoop.distribution.EHadoopVersion;
+import org.talend.hadoop.distribution.component.HBaseComponent;
+import org.talend.hadoop.distribution.component.HCatalogComponent;
+import org.talend.hadoop.distribution.component.HDFSComponent;
+import org.talend.hadoop.distribution.component.HadoopComponent;
+import org.talend.hadoop.distribution.component.HiveComponent;
+import org.talend.hadoop.distribution.component.ImpalaComponent;
+import org.talend.hadoop.distribution.component.MRComponent;
+import org.talend.hadoop.distribution.component.PigComponent;
+import org.talend.hadoop.distribution.component.SparkBatchComponent;
+import org.talend.hadoop.distribution.component.SparkStreamingComponent;
+import org.talend.hadoop.distribution.component.SqoopComponent;
+import org.talend.hadoop.distribution.mapr510.MapR510Distribution;
+
+/**
+ * Test class for the {@link MapR500Distribution} distribution.
+ *
+ */
+public class MapR510DistributionTest {
+
+    private final static String DEFAULT_YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*"; //$NON-NLS-1$
+
+    @Test
+    public void testMapR500Distribution() throws Exception {
+        HadoopComponent distribution = new MapR510Distribution();
+        assertNotNull(distribution.getDistributionName());
+        assertNotNull(distribution.getVersionName(null));
+        assertEquals(MapR510Distribution.DISTRIBUTION_NAME, distribution.getDistribution());
+        assertEquals(MapR510Distribution.VERSION, distribution.getVersion());
+        assertEquals(EHadoopVersion.HADOOP_2, distribution.getHadoopVersion());
+        assertTrue(distribution.doSupportKerberos());
+        assertFalse(distribution.doSupportUseDatanodeHostname());
+        assertTrue(distribution.doSupportGroup());
+        assertFalse(distribution.doSupportOldImportMode());
+        assertTrue(((HDFSComponent) distribution).doSupportSequenceFileShortType());
+        assertFalse(((MRComponent) distribution).isExecutedThroughWebHCat());
+        assertTrue(((MRComponent) distribution).doSupportCrossPlatformSubmission());
+        assertFalse(((MRComponent) distribution).doSupportImpersonation());
+        assertEquals(DEFAULT_YARN_APPLICATION_CLASSPATH, ((MRComponent) distribution).getYarnApplicationClasspath());
+        assertTrue(((HBaseComponent) distribution).doSupportNewHBaseAPI());
+        assertFalse(((SqoopComponent) distribution).doJavaAPISupportStorePasswordInFile());
+        assertFalse(((SqoopComponent) distribution).doJavaAPISqoopImportSupportDeleteTargetDir());
+        assertFalse(((SqoopComponent) distribution).doJavaAPISqoopImportAllTablesSupportExcludeTable());
+        assertTrue(((PigComponent) distribution).doSupportHCatalog());
+        assertFalse(((PigComponent) distribution).pigVersionPriorTo_0_12());
+        assertTrue(((PigComponent) distribution).doSupportHBase());
+        assertFalse(((PigComponent) distribution).doSupportTezForPig());
+        assertFalse(((HiveComponent) distribution).doSupportEmbeddedMode());
+        assertTrue(((HiveComponent) distribution).doSupportStandaloneMode());
+        assertFalse(((HiveComponent) distribution).doSupportHive1());
+        assertTrue(((HiveComponent) distribution).doSupportHive2());
+        assertFalse(((HiveComponent) distribution).doSupportTezForHive());
+        assertTrue(((HiveComponent) distribution).doSupportHBaseForHive());
+        assertFalse(((HiveComponent) distribution).doSupportSSL());
+        assertTrue(((HiveComponent) distribution).doSupportORCFormat());
+        assertTrue(((HiveComponent) distribution).doSupportAvroFormat());
+        assertTrue(((HiveComponent) distribution).doSupportParquetFormat());
+        assertFalse(((HiveComponent) distribution).doSupportStoreAsParquet());
+        assertFalse(((HiveComponent) distribution).doSupportClouderaNavigator());
+        assertFalse(((SparkBatchComponent) distribution).isSpark14());
+        assertTrue(((SparkBatchComponent) distribution).isSpark15());
+        assertFalse(((SparkBatchComponent) distribution).doSupportDynamicMemoryAllocation());
+        assertFalse(((SparkBatchComponent) distribution).isExecutedThroughSparkJobServer());
+        assertTrue(((SparkBatchComponent) distribution).doSupportSparkStandaloneMode());
+        assertTrue(((SparkBatchComponent) distribution).doSupportSparkYarnClientMode());
+        assertTrue(distribution instanceof SparkStreamingComponent);
+        assertFalse(((SparkStreamingComponent) distribution).isSpark14());
+        assertTrue(((SparkStreamingComponent) distribution).isSpark15());
+        assertTrue(((SparkStreamingComponent) distribution).doSupportSparkStandaloneMode());
+        assertTrue(((SparkStreamingComponent) distribution).doSupportSparkYarnClientMode());
+        assertTrue(distribution instanceof HCatalogComponent);
+        assertTrue(distribution instanceof ImpalaComponent);
+        assertTrue(((SparkStreamingComponent) distribution).doSupportCheckpointing());
+        assertFalse(((SparkStreamingComponent) distribution).doSupportBackpressure());
+    }
+
+}
---
+++ test/plugins/org.talend.hadoop.distribution.mapr510.test/src/org/talend/hadoop/distribution/mapr510/test/modulegroup/MapR510SparkBatchS3NodeModuleGroupTest.java
@@ -0,0 +1,56 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.mapr510.test.modulegroup;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.mapr510.MapR510Constant;
+import org.talend.hadoop.distribution.mapr510.modulegroup.MapR510SparkBatchS3NodeModuleGroup;
+
+/**
+ * created by pbailly on 16 Feb 2016 Detailled comment
+ *
+ */
+public class MapR510SparkBatchS3NodeModuleGroupTest {
+
+    @Test
+    public void testModuleGroups() throws Exception {
+        Map<String, String> results = new HashMap<String, String>();
+
+        results.put(MapR510Constant.SPARK_S3_MRREQUIRED_MODULE_GROUP.getModuleName(),
+                "((#LINK@NODE.STORAGE_CONFIGURATION.DISTRIBUTION=='MAPR') AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_VERSION=='MAPR510')) "
+                        + "AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_LOCAL_MODE=='false')");
+
+        Set<DistributionModuleGroup> moduleGroups = MapR510SparkBatchS3NodeModuleGroup.getModuleGroups();
+        assertEquals(results.size(), moduleGroups.size());
+        moduleGroups.iterator();
+        for (DistributionModuleGroup module : moduleGroups) {
+            assertTrue("Should contain module " + module.getModuleName(), results.containsKey(module.getModuleName()));
+            if (results.get(module.getModuleName()) == null) {
+                assertTrue("The condition of the module " + module.getModuleName() + " is not null.",
+                        results.get(module.getModuleName()) == null);
+            } else {
+                assertTrue(
+                        "The condition of the module " + module.getModuleName() + " is null, but it should be "
+                                + results.get(module.getModuleName()) + ".", results.get(module.getModuleName()) != null);
+                assertEquals(results.get(module.getModuleName()), module.getRequiredIf().getConditionString());
+            }
+        }
+    }
+}
