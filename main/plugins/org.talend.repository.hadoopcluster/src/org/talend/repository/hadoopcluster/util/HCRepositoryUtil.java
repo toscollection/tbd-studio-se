c55b662d85a958bd4601c208de6190c29a815c0c
hcyi <hcyi@talend.com>
2016-06-12 17:01:11 +0800

    TBD-3694:"Force MapR Ticket authentication" should be selected when open hbase wizard under a hadoop cluster which enable it

--- main/plugins/org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/util/HCRepositoryUtil.java
+++ main/plugins/org.talend.repository.hadoopcluster/src/org/talend/repository/hadoopcluster/util/HCRepositoryUtil.java
@@ -469,8 +469,30 @@ public class HCRepositoryUtil {
                             ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getKeytab()));
                 }
             }
+            // hbase/hive
+            map.put(ConnParameterKeys.CONN_PARA_KEY_HBASE_AUTHENTICATION_USE_MAPRTICKET,
+                    String.valueOf(hcConnection.isEnableMaprT()));
+            map.put(ConnParameterKeys.CONN_PARA_KEY_HIVE_AUTHENTICATION_USE_MAPRTICKET,
+                    String.valueOf(hcConnection.isEnableMaprT()));
+            if (hcConnection.isEnableMaprT()) {
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HBASE_AUTHENTICATION_USERNAME,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getUserName()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HBASE_AUTHENTICATION_MAPRTICKET_PASSWORD,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTPassword()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HBASE_AUTHENTICATION_MAPRTICKET_CLUSTER,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTCluster()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HBASE_AUTHENTICATION_MAPRTICKET_DURATION,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTDuration()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HIVE_AUTHENTICATION_USERNAME,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getUserName()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HIVE_AUTHENTICATION_MAPRTICKET_PASSWORD,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTPassword()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HIVE_AUTHENTICATION_MAPRTICKET_CLUSTER,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTCluster()));
+                map.put(ConnParameterKeys.CONN_PARA_KEY_HIVE_AUTHENTICATION_MAPRTICKET_DURATION,
+                        ConnectionContextHelper.getParamValueOffContext(hcConnection, hcConnection.getMaprTDuration()));
+            }
         }
-
         return map;
     }
 
