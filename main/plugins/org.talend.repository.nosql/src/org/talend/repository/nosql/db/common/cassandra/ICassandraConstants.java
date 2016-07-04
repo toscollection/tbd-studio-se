b092c4ab5ec5ecd50044c399f9a6f207e09dc52e
GL <GL@192.168.33.2>
2016-07-04 14:34:44 +0800

    TBD-3367 fix:Cassandra 3.0: Need remove API type dropdownlist should be consistent with component,do more code optimized https://jira.talendforge.org/browse/TBD-3367

--- main/plugins/org.talend.repository.nosql/src/org/talend/repository/nosql/db/common/cassandra/ICassandraConstants.java
+++ main/plugins/org.talend.repository.nosql/src/org/talend/repository/nosql/db/common/cassandra/ICassandraConstants.java
@@ -38,5 +38,12 @@ public interface ICassandraConstants {
     
     public final static String CASSANDRA200 = "CASSANDRA_2_0_0"; //$NON-NLS-1$
 
-    public final static String[] DBVERSIONS = { "CASSANDRA_3_0", "CASSANDRA_2_2","CASSANDRA_2_0_0", "CASSANDRA_1_2_2", "CASSANDRA_1_1_2" };//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
+    public final static String DB_VERSION_CASSANDRA_3_0 = "CASSANDRA_3_0";
+
+    public final static String DB_VERSION_CASSANDRA_2_2 = "CASSANDRA_2_2";
+
+    public final static String DB_VERSION_CASSANDRA_2_0_0 = "CASSANDRA_2_0_0";
+
+    public final static String[] DBVERSIONS = { DB_VERSION_CASSANDRA_3_0, DB_VERSION_CASSANDRA_2_2, DB_VERSION_CASSANDRA_2_0_0,
+            "CASSANDRA_1_2_2", "CASSANDRA_1_1_2" };//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
 }
