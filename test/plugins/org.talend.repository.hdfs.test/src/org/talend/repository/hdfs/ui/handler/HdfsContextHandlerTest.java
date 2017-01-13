35c6c0b385eb240816b4bea506bd69fe3edc5423
ycbai <ycbai@talend.com>
2017-01-13 17:11:04 +0800

    TUP-16740: add junits.

---
+++ test/plugins/org.talend.repository.hdfs.test/src/org/talend/repository/hdfs/ui/handler/HdfsContextHandlerTest.java
@@ -0,0 +1,94 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.hdfs.ui.handler;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.core.model.metadata.types.JavaTypesManager;
+import org.talend.core.model.process.IContextParameter;
+import org.talend.metadata.managment.ui.model.IConnParamName;
+import org.talend.metadata.managment.ui.utils.ConnectionContextHelper;
+import org.talend.metadata.managment.ui.utils.ExtendedNodeConnectionContextUtils.EHadoopParamName;
+import org.talend.repository.hdfs.ui.handler.testutils.HDFSConnectionTestUtils;
+import org.talend.repository.model.hdfs.HDFSConnection;
+
+/**
+ * created by ycbai on 2017年1月13日 Detailled comment
+ *
+ */
+public class HdfsContextHandlerTest {
+
+    private HdfsContextHandler contextHandler;
+
+    @Before
+    public void before() {
+        contextHandler = new HdfsContextHandler();
+    }
+
+    @Test
+    public void testCreateContextParameters() {
+        String prefixName = "testHDFS"; //$NON-NLS-1$
+        HDFSConnection connection = (HDFSConnection) HDFSConnectionTestUtils.createDefaultHDFSItem().getConnection();
+
+        Set<IConnParamName> paramSet = new HashSet<>();
+        paramSet.add(EHadoopParamName.HdfsUser);
+        paramSet.add(EHadoopParamName.HdfsFileSeparator);
+        paramSet.add(EHadoopParamName.HdfsRowSeparator);
+        paramSet.add(EHadoopParamName.HdfsRowHeader);
+
+        List<String> contextParameterNames = new ArrayList<>();
+        for (IConnParamName paramName : paramSet) {
+            contextParameterNames.add(prefixName + ConnectionContextHelper.LINE + paramName);
+        }
+
+        List<IContextParameter> contextParameters = contextHandler.createContextParameters(prefixName, connection, paramSet);
+        for (IContextParameter contextParameter : contextParameters) {
+            String contextParameterName = contextParameter.getName();
+            assertTrue(contextParameterNames.contains(contextParameterName));
+            checkContextParameter(contextParameter, connection);
+        }
+    }
+
+    private void checkContextParameter(IContextParameter contextParameter, HDFSConnection connection) {
+        String contextParameterName = contextParameter.getName();
+        String contextParameterNameWithoutPrefix = contextParameterName
+                .substring(contextParameterName.lastIndexOf(ConnectionContextHelper.LINE) + 1);
+        switch (EHadoopParamName.valueOf(contextParameterNameWithoutPrefix)) {
+        case HdfsUser:
+            assertEquals(JavaTypesManager.STRING.getId(), contextParameter.getType());
+            assertEquals(connection.getUserName(), contextParameter.getValue());
+            break;
+        case HdfsRowSeparator:
+            assertEquals(JavaTypesManager.STRING.getId(), contextParameter.getType());
+            assertEquals(connection.getRowSeparator(), contextParameter.getValue());
+            break;
+        case HdfsFileSeparator:
+            assertEquals(JavaTypesManager.STRING.getId(), contextParameter.getType());
+            assertEquals(connection.getFieldSeparator(), contextParameter.getValue());
+            break;
+        case HdfsRowHeader:
+            assertEquals(JavaTypesManager.INTEGER.getId(), contextParameter.getType());
+            assertEquals(connection.getHeaderValue(), contextParameter.getValue());
+            break;
+        default:
+        }
+    }
+
+}
---
+++ test/plugins/org.talend.repository.hdfs.test/src/org/talend/repository/hdfs/ui/handler/testutils/HDFSConnectionTestUtils.java
@@ -0,0 +1,57 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.hdfs.ui.handler.testutils;
+
+import org.talend.commons.utils.VersionUtils;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.properties.PropertiesFactory;
+import org.talend.core.model.properties.Property;
+import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.repository.model.hdfs.HDFSConnection;
+import org.talend.repository.model.hdfs.HDFSConnectionItem;
+import org.talend.repository.model.hdfs.HDFSFactory;
+
+/**
+ * created by ycbai on 2017年1月13日 Detailled comment
+ *
+ */
+public class HDFSConnectionTestUtils {
+
+    public static HDFSConnectionItem createDefaultHDFSItem() {
+        return createHDFSItem("testId", "testHDFS"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+
+    public static HDFSConnectionItem createHDFSItem(String id, String name) {
+        HDFSConnection connection = HDFSFactory.eINSTANCE.createHDFSConnection();
+        connection.setRowSeparator("\\n"); //$NON-NLS-1$
+        connection.setFieldSeparator(";"); //$NON-NLS-1$
+        connection.setHeaderValue("2"); //$NON-NLS-1$
+
+        Property connectionProperty = PropertiesFactory.eINSTANCE.createProperty();
+        connectionProperty.setAuthor(
+                ((RepositoryContext) CoreRuntimePlugin.getInstance().getContext().getProperty(Context.REPOSITORY_CONTEXT_KEY))
+                        .getUser());
+        connectionProperty.setVersion(VersionUtils.DEFAULT_VERSION);
+        connectionProperty.setStatusCode(""); //$NON-NLS-1$
+        connectionProperty.setId(id);
+        connectionProperty.setLabel(name);
+
+        HDFSConnectionItem connectionItem = HDFSFactory.eINSTANCE.createHDFSConnectionItem();
+        connectionItem.setProperty(connectionProperty);
+        connectionItem.setConnection(connection);
+
+        return connectionItem;
+    }
+
+}
