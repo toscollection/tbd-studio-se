944053733bac5fbee548eeda312c227d64688d53
hcyi <hcyi@talend.com>
2017-05-22 16:10:49 +0800

    TUP-17114:Cann't retrieve schma for HDFS when studio is on EC2 instance (#320)
    when EC2 and EMR5 are in same VPC

---
+++ main/plugins/org.talend.designer.hdfsbrowse/src/org/talend/designer/hdfsbrowse/model/EHadoopAdditionalJars.java
@@ -0,0 +1,51 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.hdfsbrowse.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * created by hcyi on May 18, 2017 Detailled comment
+ *
+ */
+public enum EHadoopAdditionalJars {
+
+    LZO_ID("io.compression.codec.lzo.class")//$NON-NLS-1$ 
+
+    ;
+
+    private String value;
+
+    EHadoopAdditionalJars(String value) {
+        this.value = value;
+    }
+
+    public static List<String> getBaseLoaderClassIds() {
+        List<String> vals = new ArrayList<String>();
+        EHadoopAdditionalJars[] values = values();
+        for (EHadoopAdditionalJars value : values) {
+            vals.add(value.getValue());
+        }
+        return vals;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public String getName() {
+        return this.name();
+    }
+}
