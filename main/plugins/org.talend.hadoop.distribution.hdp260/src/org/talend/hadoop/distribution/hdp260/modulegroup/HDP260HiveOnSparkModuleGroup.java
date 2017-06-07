6470a9a091b1a965143af97811e2a3da0bb20719
lbourgeois <lbourgeois@talend.com>
2017-06-07 09:51:25 +0200

    TBD-4991 HDP260 Fix Hive on Spark dependencies (#353)
    * TBD-4991 Fix component conditions for HiveOnSpark dependencies
    
    * TBD-4991 Remove commons-lang from hive-exec
    
    * TBD-4991 Code cleaning

--- main/plugins/org.talend.hadoop.distribution.hdp260/src/org/talend/hadoop/distribution/hdp260/modulegroup/HDP260HiveOnSparkModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.hdp260/src/org/talend/hadoop/distribution/hdp260/modulegroup/HDP260HiveOnSparkModuleGroup.java
@@ -17,30 +17,34 @@ import java.util.Set;
 
 import org.talend.hadoop.distribution.DistributionModuleGroup;
 import org.talend.hadoop.distribution.ESparkVersion;
-import org.talend.hadoop.distribution.condition.BasicExpression;
 import org.talend.hadoop.distribution.condition.BooleanOperator;
 import org.talend.hadoop.distribution.condition.ComponentCondition;
 import org.talend.hadoop.distribution.condition.EqualityOperator;
+import org.talend.hadoop.distribution.condition.LinkedNodeExpression;
 import org.talend.hadoop.distribution.condition.MultiComponentCondition;
 import org.talend.hadoop.distribution.constants.SparkBatchConstant;
 import org.talend.hadoop.distribution.hdp260.HDP260Constant;
 
 public class HDP260HiveOnSparkModuleGroup {
 
-    private final static ComponentCondition spark16Condition = new MultiComponentCondition(new BasicExpression(
-            SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER, EqualityOperator.EQ, "false"), BooleanOperator.AND,
-            new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_1_6.getSparkVersion()));
+    private final static ComponentCondition spark16Condition = new MultiComponentCondition(new LinkedNodeExpression(
+            SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER, SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER,
+            EqualityOperator.EQ, "false"), BooleanOperator.AND, new LinkedNodeExpression(
+            SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER, "SUPPORTED_SPARK_VERSION", EqualityOperator.EQ,
+            ESparkVersion.SPARK_1_6.getSparkVersion()));
 
-    private final static ComponentCondition spark21Condition = new MultiComponentCondition(new BasicExpression(
-            SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER, EqualityOperator.EQ, "false"), BooleanOperator.AND,
-            new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_2_1.getSparkVersion()));
+    private final static ComponentCondition spark21Condition = new MultiComponentCondition(new LinkedNodeExpression(
+            SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER, SparkBatchConstant.SPARK_LOCAL_MODE_PARAMETER,
+            EqualityOperator.EQ, "false"), BooleanOperator.AND, new LinkedNodeExpression(
+            SparkBatchConstant.SPARK_BATCH_SPARKCONFIGURATION_LINKEDPARAMETER, "SUPPORTED_SPARK_VERSION", EqualityOperator.EQ,
+            ESparkVersion.SPARK_2_1.getSparkVersion()));
 
     public static Set<DistributionModuleGroup> getModuleGroups() {
         Set<DistributionModuleGroup> hs = new HashSet<>();
         DistributionModuleGroup dmgSpark16 = new DistributionModuleGroup(
                 HDP260Constant.SPARK_HIVE_MRREQUIRED_MODULE_GROUP.getModuleName(), true, spark16Condition);
         DistributionModuleGroup dmgSpark21 = new DistributionModuleGroup(
-                HDP260Constant.SPARK2_HIVE_MRREQUIRED_MODULE_GROUP.getModuleName(), true, spark16Condition);
+                HDP260Constant.SPARK2_HIVE_MRREQUIRED_MODULE_GROUP.getModuleName(), true, spark21Condition);
         hs.add(new DistributionModuleGroup(HDP260Constant.TEZ_MODULE_GROUP.getModuleName(), false, null));
         hs.add(dmgSpark16);
         hs.add(dmgSpark21);
