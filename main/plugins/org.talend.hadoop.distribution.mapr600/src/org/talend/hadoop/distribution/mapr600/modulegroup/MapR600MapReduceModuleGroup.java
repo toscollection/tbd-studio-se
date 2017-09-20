58674cbff80ca6c5e5712ed9accb2807d91a2979
Chen Bin <bchen@talend.com>
2017-09-20 08:54:06 -0500

    feat(TBD-5410): new MapR 6.0 distribution Spark batch only (#455)
    * feat(TBD-5586): init commit for new MapR 6.0 distribution
    
    * feat(TBD-5586): configure jars in plugin.xml
    1. copy plugin.xml from 5.2
    2. replace all jars which can be find in MapR 6.0 and replace them
    3. do not replace spark jar, as in 5.2 it's assembly jar, but in 6.0
    it's not
    4. get spark 2.1 version of spark-streaming-kinesis
    
    * feat(TBD-5586): add test plugin
    
    * pom added
    
    * feat(TBD-5586): add spark2 libs
    
    * Hbase functionality turned on
    
    * correct test plugin path
    
    * feat(TBD-5653): pin the version of netty-all for MapRDB components
    
    * fix build issue
    
    * yarn classpath corrected
    
    * MapReduce activated for mapr6
    
    * Revert "MapReduce activated for mapr6"
    
    This reverts commit 3d40d8617db29899fd8a8d4b4eb2ddd31dc37c4d.
    
    * correct junit
    
    * modify version with Beta
    
    * add required flexjson jar for yarn cluster kerberos
    
    * all mvn uris now set to 6.0.0

---
+++ main/plugins/org.talend.hadoop.distribution.mapr600/src/org/talend/hadoop/distribution/mapr600/modulegroup/MapR600MapReduceModuleGroup.java
@@ -0,0 +1,30 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.mapr600.modulegroup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.mapr600.MapR600Constant;
+
+public class MapR600MapReduceModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroups() {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        hs.add(new DistributionModuleGroup(MapR600Constant.HDFS_MODULE_GROUP.getModuleName()));
+        hs.add(new DistributionModuleGroup(MapR600Constant.MAPREDUCE_MODULE_GROUP.getModuleName()));
+        hs.add(new DistributionModuleGroup(MapR600Constant.MAPREDUCE_PARQUET_MODULE_GROUP.getModuleName(), true, null));
+        return hs;
+    }
+}
