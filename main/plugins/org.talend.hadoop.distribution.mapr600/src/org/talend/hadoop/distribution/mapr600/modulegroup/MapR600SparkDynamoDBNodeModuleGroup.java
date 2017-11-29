2534b388ff53b714d328c72ff65c3cd17cec788d
Chen Bin <bchen@talend.com>
2017-11-29 06:49:54 -0600

    fix(TBD-5470): master fix (#594)

--- main/plugins/org.talend.hadoop.distribution.mapr600/src/org/talend/hadoop/distribution/mapr600/modulegroup/MapR600SparkDynamoDBNodeModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.mapr600/src/org/talend/hadoop/distribution/mapr600/modulegroup/MapR600SparkDynamoDBNodeModuleGroup.java
@@ -4,13 +4,13 @@ import java.util.Set;
 
 import org.talend.hadoop.distribution.DistributionModuleGroup;
 import org.talend.hadoop.distribution.mapr600.MapR600Constant;
-import org.talend.hadoop.distribution.utils.dynamodb.SparkDynamoDBNodeModuleGroupUtil;
+import org.talend.hadoop.distribution.utils.ModuleGroupsUtils;
 
 public class MapR600SparkDynamoDBNodeModuleGroup {
 
     public static Set<DistributionModuleGroup> getModuleGroups(String distribution, String version, String condition) {
-        return SparkDynamoDBNodeModuleGroupUtil.getModuleGroups(distribution, version, condition,
-                MapR600Constant.SPARK_DYNAMODB_MRREQUIRED_MODULE_GROUP.getModuleName());
+        return ModuleGroupsUtils.getModuleGroups(distribution, version, condition,
+                MapR600Constant.SPARK_DYNAMODB_MRREQUIRED_MODULE_GROUP.getModuleName(), true);
     }
 
 }
