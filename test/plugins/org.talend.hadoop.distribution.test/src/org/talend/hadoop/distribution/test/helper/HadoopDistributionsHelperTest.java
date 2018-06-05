565b92193f71b218416eb5b80398c263226fba64
Oleh Stoliarov <36441857+ostoliarov@users.noreply.github.com>
2018-06-05 16:19:49 +0300

    Fix Qubole related unit tests (#795)

--- test/plugins/org.talend.hadoop.distribution.test/src/org/talend/hadoop/distribution/test/helper/HadoopDistributionsHelperTest.java
+++ test/plugins/org.talend.hadoop.distribution.test/src/org/talend/hadoop/distribution/test/helper/HadoopDistributionsHelperTest.java
@@ -28,14 +28,14 @@ public class HadoopDistributionsHelperTest {
     public void testGetHadoopDistributionsDisplay_Sorted() {
         String[] hadoopDistributionsDisplay = HadoopDistributionsHelper.HADOOP.getDistributionsDisplay(false);
         Assert.assertArrayEquals(new String[] { "Amazon EMR", "Cloudera", "Google Cloud Dataproc",
-                "HortonWorks", "MapR", "Microsoft HD Insight" }, hadoopDistributionsDisplay);
+                "HortonWorks", "MapR", "Microsoft HD Insight", "Qubole" }, hadoopDistributionsDisplay);
     }
 
     @Test
     public void testGetHadoopDistributionsDisplay_withCustom__Sorted() {
         String[] hadoopDistributionsDisplay = HadoopDistributionsHelper.HADOOP.getDistributionsDisplay(true);
         Assert.assertArrayEquals(new String[] { "Amazon EMR", "Cloudera", "Google Cloud Dataproc",
-                "HortonWorks", "MapR", "Microsoft HD Insight", "Custom - Unsupported" },
+                "HortonWorks", "MapR", "Microsoft HD Insight", "Qubole", "Custom - Unsupported" },
                 hadoopDistributionsDisplay);
     }
 
