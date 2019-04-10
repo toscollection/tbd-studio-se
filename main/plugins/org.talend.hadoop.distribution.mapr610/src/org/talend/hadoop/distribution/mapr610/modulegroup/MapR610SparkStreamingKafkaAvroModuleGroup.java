c3af42eb10a22b8bf6e40e1d924600a665909cd9
mtuhai <mtuhai@talend.com>
2019-04-10 23:26:50 +0300

    Feat/mapr610 di (#994)
    * Initial commit
    
    * pom updated
    
    * copyright fixed
    
    * yarn classpath fixed
    
    * plugin.xml for HDFS DI ready
    
    * jar versions corrected
    
    * pom.xml corrected
    
    * hadoop-aws for spark
    
    * plugin.xml mostly finished
    
    * Avro fix for sqoop
    
    * Pig fixes
    
    * TBD-8541: in process
    
    * Code review changes

---
+++ main/plugins/org.talend.hadoop.distribution.mapr610/src/org/talend/hadoop/distribution/mapr610/modulegroup/MapR610SparkStreamingKafkaAvroModuleGroup.java
@@ -0,0 +1,38 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.mapr610.modulegroup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.core.hadoop.version.EHadoopDistributions;
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.condition.common.SparkStreamingLinkedNodeCondition;
+import org.talend.hadoop.distribution.constants.SparkStreamingConstant;
+import org.talend.hadoop.distribution.mapr610.MapR610Constant;
+import org.talend.hadoop.distribution.mapr610.MapR610Distribution;
+
+public class MapR610SparkStreamingKafkaAvroModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroups() {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(
+                MapR610Constant.SPARK_KAFKA_AVRO_MRREQUIRED_MODULE_GROUP.getModuleName(), true,
+                new SparkStreamingLinkedNodeCondition(EHadoopDistributions.MAPR.getName(), MapR610Distribution.VERSION,
+                        SparkStreamingConstant.KAFKA_SPARKCONFIGURATION_LINKEDPARAMETER).getCondition());
+        hs.add(dmg);
+        // Add Spark Streaming Kafka dependencies as well
+        hs.addAll(MapR610SparkStreamingKafkaAssemblyModuleGroup.getModuleGroups());
+        return hs;
+    }
+}
