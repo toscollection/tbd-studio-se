b5dba897a9cdbe9a6cb8a8a7d432ea34b200ae79
Laurent BOURGEOIS <lbourgeois@talend.com>
2019-05-22 18:36:23 +0200

    feat(TBD-8427): CDH 6.1.1 spark batch (#1016)

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/modulegroup/DynamicSparkStreamingModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/modulegroup/DynamicSparkStreamingModuleGroup.java
@@ -55,7 +55,11 @@ public class DynamicSparkStreamingModuleGroup extends AbstractModuleGroup {
                                 new MultiComponentCondition(
                                         new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_2_2.getSparkVersion()), //$NON-NLS-1$
                                         BooleanOperator.OR,
-                                        new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_2_3.getSparkVersion()) //$NON-NLS-1$
+                                        new MultiComponentCondition(
+                                                new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_2_3.getSparkVersion()), //$NON-NLS-1$
+                                                BooleanOperator.OR,
+                                                new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.EQ, ESparkVersion.SPARK_2_4.getSparkVersion()) //$NON-NLS-1$
+                                        )
                                 )
                         )
                 ));
