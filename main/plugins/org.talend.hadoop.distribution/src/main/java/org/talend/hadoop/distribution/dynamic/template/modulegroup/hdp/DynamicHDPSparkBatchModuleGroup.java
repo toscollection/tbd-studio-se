db9d0af56338945c59e163316d1ae34c7b2ecdd5
sponomarova <42870019+sponomarova@users.noreply.github.com>
2019-07-26 13:04:05 +0300

    fix(TBD-8565): skipping jackson-databind-2.6.6 for hdp2603-8 built-in distribution and Spark engine version 1.6 (#1074)

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/modulegroup/hdp/DynamicHDPSparkBatchModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/modulegroup/hdp/DynamicHDPSparkBatchModuleGroup.java
@@ -17,9 +17,11 @@ import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.ESparkVersion;
 import org.talend.hadoop.distribution.condition.BasicExpression;
 import org.talend.hadoop.distribution.condition.BooleanOperator;
 import org.talend.hadoop.distribution.condition.ComponentCondition;
+import org.talend.hadoop.distribution.condition.EqualityOperator;
 import org.talend.hadoop.distribution.condition.MultiComponentCondition;
 import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
 import org.talend.hadoop.distribution.constants.MRConstant;
@@ -33,8 +35,17 @@ import org.talend.hadoop.distribution.dynamic.template.modulegroup.DynamicSparkB
  */
 public class DynamicHDPSparkBatchModuleGroup extends DynamicSparkBatchModuleGroup {
 
+    protected ComponentCondition conditionNotSpark16;
+
     public DynamicHDPSparkBatchModuleGroup(DynamicPluginAdapter pluginAdapter) {
         super(pluginAdapter);
+    }
+
+    @Override
+    protected void initConditions() {
+        super.initConditions();
+        conditionNotSpark16 = new SimpleComponentCondition(
+                new BasicExpression("SUPPORTED_SPARK_VERSION", EqualityOperator.NOT_EQ, ESparkVersion.SPARK_1_6.getSparkVersion())); //$NON-NLS-1$        
     }
 
     @Override
@@ -52,6 +63,10 @@ public class DynamicHDPSparkBatchModuleGroup extends DynamicSparkBatchModuleGrou
                 .getRuntimeModuleGroupIdByTemplateId(DynamicModuleGroupConstant.SPARK_MRREQUIRED_MODULE_GROUP.getModuleName());
         String hdfsRuntimeId = pluginAdapter
                 .getRuntimeModuleGroupIdByTemplateId(DynamicModuleGroupConstant.HDFS_MODULE_GROUP.getModuleName());
+        String hdfsNotSpark16RuntimeId = pluginAdapter
+                .getRuntimeModuleGroupIdByTemplateId(DynamicModuleGroupConstant.HDFS_NOT_SPARK_1_6_MODULE_GROUP.getModuleName());
+        String tezNotSpark16RuntimeId = pluginAdapter
+                .getRuntimeModuleGroupIdByTemplateId(DynamicModuleGroupConstant.TEZ_NOT_SPARK_1_6_MODULE_GROUP.getModuleName());
         String mapReduceRuntimeId = pluginAdapter
                 .getRuntimeModuleGroupIdByTemplateId(DynamicModuleGroupConstant.MAPREDUCE_MODULE_GROUP.getModuleName());
         String atlasSpark1RuntimeId = pluginAdapter
@@ -62,6 +77,8 @@ public class DynamicHDPSparkBatchModuleGroup extends DynamicSparkBatchModuleGrou
         checkRuntimeId(spark2RuntimeId);
         checkRuntimeId(sparkMRRequiredRuntimeId);
         checkRuntimeId(hdfsRuntimeId);
+        checkRuntimeId(hdfsNotSpark16RuntimeId);
+        checkRuntimeId(tezNotSpark16RuntimeId);
         checkRuntimeId(mapReduceRuntimeId);
         checkRuntimeId(atlasSpark1RuntimeId);
         checkRuntimeId(atlasSpark2RuntimeId);
@@ -77,6 +94,12 @@ public class DynamicHDPSparkBatchModuleGroup extends DynamicSparkBatchModuleGrou
         if (StringUtils.isNotBlank(hdfsRuntimeId)) {
             moduleGroups.add(new DistributionModuleGroup(hdfsRuntimeId, false, conditionSpark1));
             moduleGroups.add(new DistributionModuleGroup(hdfsRuntimeId, false, conditionSpark2));
+        }
+        if (StringUtils.isNotBlank(hdfsNotSpark16RuntimeId)) {
+            moduleGroups.add(new DistributionModuleGroup(hdfsNotSpark16RuntimeId, false, conditionNotSpark16));
+        }
+        if (StringUtils.isNotBlank(tezNotSpark16RuntimeId)) {
+            moduleGroups.add(new DistributionModuleGroup(tezNotSpark16RuntimeId, false, conditionNotSpark16));
         }
         if (StringUtils.isNotBlank(mapReduceRuntimeId)) {
             moduleGroups.add(new DistributionModuleGroup(mapReduceRuntimeId, false, conditionSpark1));
