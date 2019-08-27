b2c106574c8507c2b88a264531be82baf56dadf2
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2019-08-27 16:47:20 +0800

    fix(TUP-24330) Cannot retrieve conf from cdh512 (#1097)
    fix(TUP-24330) Can not retrieve conf from CDH512

--- main/plugins/org.talend.repository.hadoopcluster.configurator/src/org/talend/repository/hadoopcluster/configurator/cloudera/HadoopCMConfigurator.java
+++ main/plugins/org.talend.repository.hadoopcluster.configurator/src/org/talend/repository/hadoopcluster/configurator/cloudera/HadoopCMConfigurator.java
@@ -13,12 +13,17 @@
 package org.talend.repository.hadoopcluster.configurator.cloudera;
 
 import java.io.ByteArrayInputStream;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -30,9 +35,12 @@ import com.cloudera.api.swagger.ClustersResourceApi;
 import com.cloudera.api.swagger.ServicesResourceApi;
 import com.cloudera.api.swagger.client.ApiClient;
 import com.cloudera.api.swagger.client.ApiException;
+import com.cloudera.api.swagger.client.ApiResponse;
 import com.cloudera.api.swagger.client.Configuration;
 import com.cloudera.api.swagger.model.ApiCluster;
 import com.cloudera.api.swagger.model.ApiClusterList;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.Call;
 
 /**
  * created by bchen on May 28, 2015 Detailled comment
@@ -43,7 +51,9 @@ public class HadoopCMConfigurator implements HadoopConfigurator {
     static final String CERT_BEGIN = "-----BEGIN CERTIFICATE-----";
     static final String CERT_END = "-----END CERTIFICATE-----";
     static final char SEPARATOR = '\n';
-    static final String API_VERSION = "v18";
+
+    Logger log = Logger.getLogger(HadoopCMConfigurator.class.getCanonicalName());
+    static final String DEFAULT_API_VERSION = "v1";
 
     ServicesResourceApi serviceAPI;
 
@@ -53,11 +63,44 @@ public class HadoopCMConfigurator implements HadoopConfigurator {
         ApiClient apiClient;
         try {
             apiClient = createClient(build);
+            setAPIVersion(apiClient);
             serviceAPI = new ServicesResourceApi(apiClient);
             clusterAPI = new ClustersResourceApi(apiClient);
-        } catch (CertificateEncodingException e) {
+        } catch (CertificateEncodingException | ApiException e) {
+            log.log(Level.SEVERE, "HadoopCMConfigurator error", e);
             throw new RuntimeException(e);
         }
+    }
+
+    private void setAPIVersion(ApiClient client) throws ApiException {
+        String requestPath = "/version";
+        String method="GET";
+        Map<String, String> headerParams = new HashMap<String, String>();
+        
+        String[] accepts = { "application/json" };
+
+        String accept = client.selectHeaderAccept(accepts);
+        if (accept != null) headerParams.put("Accept", accept);
+        
+        String[] contentTypes = new String[0];
+        String[] authNames = { "basic" };
+        String contentType = client.selectHeaderContentType(contentTypes);
+        headerParams.put("Content-Type", contentType);
+        Call c =client.buildCall(requestPath, method, null, null, headerParams, null, authNames, null);
+        Type returnType = (new TypeToken<String>() {  }).getType();
+        ApiResponse<String> version = client.execute(c, returnType);
+
+        if (version.getStatusCode() == 200) {
+            // highest version
+            client.setBasePath(client.getBasePath() + "/" + version.getData());
+        } else if (version.getStatusCode() == 404) {
+            // default version
+            client.setBasePath(client.getBasePath() + "/" + DEFAULT_API_VERSION);
+        } else {
+            // throw exception
+            throw new RuntimeException("Can't retrieve api version from " + client.getBasePath());
+        }
+        log.log(Level.INFO, "setAPIVersion, base path: " + client.getBasePath());
     }
 
     private ApiClient createClient(Builder build) throws CertificateEncodingException {
@@ -67,8 +110,7 @@ public class HadoopCMConfigurator implements HadoopConfigurator {
         if (!sb.toString().endsWith("/")) {
             sb.append("/");
         }
-        sb.append("api/");
-        sb.append(API_VERSION);
+        sb.append("api");
         cmClient.setBasePath(sb.toString());
         cmClient.setUsername(build.user);
         cmClient.setPassword(build.password);
@@ -147,7 +189,7 @@ public class HadoopCMConfigurator implements HadoopConfigurator {
         ApiClusterList clusters;
         try {
             clusters = clusterAPI.readClusters(null, HadoopCMCluster.DEFAULT_VIEW_NAME);
-            System.out.println(clusters.toString());
+
             for (ApiCluster cluster : clusters.getItems()) {
                 names.add(cluster.getDisplayName() + NAME_SEPARATOR + cluster.getName());
             }
