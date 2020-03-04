693669311fd75ee9eb18c1b9b29b5a7e9dc281f8
rhaddou <rhaddou@talend.com>
2020-03-04 16:35:00 +0100

    init cdp703

---
+++ main/plugins/org.talend.hadoop.distribution.cdp703/src/main/java/org/talend/hadoop/distribution/cdp703/modulegroup/node/sparkstreaming/CDP703SparkStreamingKafkaAvroModuleGroup.java
@@ -0,0 +1,36 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.cdp703.modulegroup.node.sparkstreaming;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.cdp703.CDP703Constant;
+import org.talend.hadoop.distribution.condition.common.SparkStreamingLinkedNodeCondition;
+import org.talend.hadoop.distribution.constants.SparkStreamingConstant;
+
+public class CDP703SparkStreamingKafkaAvroModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroups(String distribution, String version) {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(
+                CDP703Constant.SPARK_KAFKA_AVRO_MRREQUIRED_MODULE_GROUP.getModuleName(), true,
+                new SparkStreamingLinkedNodeCondition(distribution, version,
+                        SparkStreamingConstant.KAFKA_SPARKCONFIGURATION_LINKEDPARAMETER).getCondition());
+        hs.add(dmg);
+        // Add Spark Streaming Kafka dependencies as well
+        hs.addAll(CDP703SparkStreamingKafkaAssemblyModuleGroup.getModuleGroups(distribution, version));
+        return hs;
+    }
+}
