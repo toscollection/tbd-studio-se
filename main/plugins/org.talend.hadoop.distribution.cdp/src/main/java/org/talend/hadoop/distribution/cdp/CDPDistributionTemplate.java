2a6bf62dfbe5a7ad245e494495f27e0ac64d86ae
rhaddou <rhaddou@talend.com>
2020-03-20 13:47:25 +0100

    yarn class path

--- main/plugins/org.talend.hadoop.distribution.cdp/src/main/java/org/talend/hadoop/distribution/cdp/CDPDistributionTemplate.java
+++ main/plugins/org.talend.hadoop.distribution.cdp/src/main/java/org/talend/hadoop/distribution/cdp/CDPDistributionTemplate.java
@@ -10,14 +10,15 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
+
 package org.talend.hadoop.distribution.cdp;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.talend.hadoop.distribution.ESparkVersion;
-import org.talend.hadoop.distribution.ESqoopPackageName;
-import org.talend.hadoop.distribution.component.CDHSparkBatchComponent;
+import org.talend.hadoop.distribution.component.CDPSparkBatchComponent;
 import org.talend.hadoop.distribution.component.HBaseComponent;
 import org.talend.hadoop.distribution.component.HCatalogComponent;
 import org.talend.hadoop.distribution.component.HDFSComponent;
@@ -28,21 +29,21 @@ import org.talend.hadoop.distribution.component.MRComponent;
 import org.talend.hadoop.distribution.component.SparkStreamingComponent;
 import org.talend.hadoop.distribution.component.SqoopComponent;
 import org.talend.hadoop.distribution.dynamic.adapter.DynamicPluginAdapter;
-import org.talend.hadoop.distribution.dynamic.template.cdh.AbstractDynamicCDHDistributionTemplate;
+import org.talend.hadoop.distribution.dynamic.template.cdp.AbstractDynamicCDPDistributionTemplate;
 import org.talend.hadoop.distribution.kudu.KuduVersion;
 
-
-/**
- * DOC cmeng  class global comment. Detailled comment
- */
 @SuppressWarnings("nls")
-public class CDPDistributionTemplate extends AbstractDynamicCDHDistributionTemplate implements HDFSComponent, HBaseComponent,
- HCatalogComponent, MRComponent, HiveComponent, HiveOnSparkComponent,
-        ImpalaComponent, SqoopComponent, CDHSparkBatchComponent, SparkStreamingComponent, ICDPDistributionTemplate {
+public class CDPDistributionTemplate extends AbstractDynamicCDPDistributionTemplate implements HDFSComponent, HBaseComponent,
+        HCatalogComponent, MRComponent, HiveComponent, HiveOnSparkComponent, ImpalaComponent, SqoopComponent,
+ CDPSparkBatchComponent, SparkStreamingComponent, ICDPDistributionTemplate {
 
     public final static String TEMPLATE_ID = "CDPDistributionTemplate";
+   //private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*"; //$NON-NLS-1$
 
-    private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*"; //$NON-NLS-1$
+    private final static String YARN_APPLICATION_CLASSPATH = "/opt/cloudera/parcels/CDH/lib/spark/jars/*," + 
+    		"/opt/cloudera/parcels/CDH/lib/hive/lib/*," + 
+    		"/opt/cloudera/parcels/CDH/lib/impala/lib/*";
+    		//+ "/opt/cloudera/parcels/CDH/lib/hadoop-hdfs/*"; 
 
     public CDPDistributionTemplate(DynamicPluginAdapter pluginAdapter) throws Exception {
         super(pluginAdapter);
@@ -54,30 +55,46 @@ public class CDPDistributionTemplate extends AbstractDynamicCDHDistributionTempl
     }
 
     @Override
-    public boolean doJavaAPISupportStorePasswordInFile() {
-        return true;
-    }
-
-    @Override
-    public boolean doJavaAPISqoopImportSupportDeleteTargetDir() {
-        return true;
+    public String getYarnApplicationClasspath() {
+        return YARN_APPLICATION_CLASSPATH;
     }
     
     @Override
-    public boolean doSupportParquetOutput() {
+    public String generateSparkJarsPaths(List<String> commandLineJarsPaths) {
+    	System.out.println(commandLineJarsPaths);
+    	
+        return getYarnApplicationClasspath() ;
+    }
+    @Override
+    public boolean doSupportSequenceFileShortType() {
         return true;
     }
 
     @Override
-    public boolean doJavaAPISqoopImportAllTablesSupportExcludeTable() {
-        return true;
-    }
-    
-    @Override
-    public boolean doSupportClouderaNavigator() {
+    public boolean doSupportNewHBaseAPI() {
         return true;
     }
 
+    @Override
+    public boolean doSupportCrossPlatformSubmission() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportImpersonation() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportEmbeddedMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportStandaloneMode() {
+        return super.doSupportStandaloneMode();
+    }
+   
     @Override
     public boolean doSupportHive1() {
         return false;
@@ -104,14 +121,6 @@ public class CDPDistributionTemplate extends AbstractDynamicCDHDistributionTempl
     }
 
     @Override
-    /**
-     * Added in Studio 7.2.1 for CDH 6.x
-     */
-    public boolean doImpalaSupportSSL() {
-        return true;
-    }
-
-    @Override
     public boolean doSupportORCFormat() {
         return true;
     }
@@ -132,127 +141,36 @@ public class CDPDistributionTemplate extends AbstractDynamicCDHDistributionTempl
     }
 
     @Override
-    public boolean doSupportCrossPlatformSubmission() {
+    public boolean doJavaAPISupportStorePasswordInFile() {
         return true;
     }
 
     @Override
-    public boolean doSupportImpersonation() {
-        return true;
-    }
-    
-    @Override
-    public boolean doSupportEmbeddedMode() {
-        return false;
-    }
-    
-    @Override
-    public boolean doSupportStandaloneMode() {
-        return super.doSupportStandaloneMode();
-    }
-
-    @Override
-    public boolean doSupportNewHBaseAPI() {
+    public boolean doJavaAPISqoopImportSupportDeleteTargetDir() {
         return true;
     }
 
     @Override
-    public boolean doSupportSequenceFileShortType() {
+    public boolean doJavaAPISqoopImportAllTablesSupportExcludeTable() {
         return true;
     }
 
     @Override
-    public boolean doSupportHBase2x() {
+    public boolean doSupportClouderaNavigator() {
         return true;
     }
 
     @Override
-    public String getYarnApplicationClasspath() {
-        return YARN_APPLICATION_CLASSPATH;
-    }
-
-    @Override
-    public short orderingWeight() {
-        return 15;
-    }
-
-    @Override
-    public boolean doImportDynamoDBDependencies() {
-        return true;
-    }
-    
-    @Override
-    public boolean doSupportAssumeRole() {
-        return true;
-    }
-    
-    @Override
-    public boolean useOldAWSAPI() {
-        return false;
-    }
-    
-    @Override
-    public boolean doSupportAvroDeflateProperties() {
-        return true;
-    }
-    
-    @Override
-    public String getSqoopPackageName() {
-        return ESqoopPackageName.ORG_APACHE_SQOOP.toString();
-    }
-
-    @Override
-    public boolean doSupportSSLwithKerberos() {
+    public boolean doSupportParquetOutput() {
         return true;
     }
 
-	@Override
-	public boolean doSupportSparkStandaloneMode() {
-		return true;
-	}
-
-	@Override
-	public boolean doSupportSparkYarnClientMode() {
-		return true;
-	}
-
-	@Override
-	public boolean doSupportDynamicMemoryAllocation() {
-		return true;
-	}
-	
     @Override
-    public boolean isExecutedThroughSparkJobServer() {
-        return false;
-    }
-    
-    @Override
-    public boolean doSupportAzureBlobStorage() {
-        return true;
-    }
-    
-    @Override
-    public int getClouderaNavigatorAPIVersion() {
-        return 13;
-    }
-
-
-	@Override
-	public boolean doSupportCheckpointing() {
-		return true;
-	}
-
-	@Override
-	public boolean doSupportBackpressure() {
-		return true;
-	}
-	
-	@Override
     public Set<ESparkVersion> getSparkVersions() {
         Set<ESparkVersion> version = new HashSet<>();
         Set<ESparkVersion> sparkVersions = super.getSparkVersions();
         if (sparkVersions == null || sparkVersions.isEmpty()) {
-            version.add(ESparkVersion.SPARK_2_4);
+            version.add(ESparkVersion.SPARK_2_2);
         } else {
             version.addAll(sparkVersions);
         }
@@ -260,17 +178,77 @@ public class CDPDistributionTemplate extends AbstractDynamicCDHDistributionTempl
     }
 
     @Override
-    public boolean useS3AProperties() {
+    public boolean isExecutedThroughSparkJobServer() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportSparkStandaloneMode() {
         return true;
     }
 
     @Override
-    public KuduVersion getKuduVersion() {
-        return KuduVersion.KUDU_1_8;
+    public boolean doSupportSparkYarnClientMode() {
+        return true;
     }
-    
+
     @Override
-    public boolean doSupportAzureDataLakeStorageGen2() {
-    	return true;
+    public boolean doSupportDynamicMemoryAllocation() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportSSLwithKerberos() {
+        return true;
+    }
+
+    @Override
+    public int getClouderaNavigatorAPIVersion() {
+        return 9;
+    }
+
+    @Override
+    public boolean doSupportCheckpointing() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportBackpressure() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAzureBlobStorage() {
+        return true;
+    }
+
+    @Override
+    public short orderingWeight() {
+        return 10;
+    }
+
+    @Override
+    public boolean doImportDynamoDBDependencies() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAssumeRole() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAvroDeflateProperties() {
+        return true;
+    }
+
+    @Override
+    public boolean useOldAWSAPI() {
+        return false;
+    }
+
+    @Override
+    public KuduVersion getKuduVersion() {
+        return KuduVersion.KUDU_1_7;
     }
 }
