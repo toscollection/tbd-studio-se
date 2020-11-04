93c8b7be20bc4722a3eabc7455a3fa5cca912c13
Svitlana Ponomarova <sponomarova@talend.com>
2020-11-04 17:22:40 +0200

    fix(TBD-10086): add fix from TBD-11543

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/AbstractDistribution.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/AbstractDistribution.java
@@ -255,7 +255,7 @@ public abstract class AbstractDistribution {
     }
 
     public String generateSparkJarsPaths(List<String> commandLineJarsPaths) {
-        return ""; //$NON-NLS-1$
+    	return generateSparkJarsPaths(commandLineJarsPaths);
     }
     
     public String generateSparkJarsPaths(List<String> commandLineJarsPaths, boolean isLightWeight) {
