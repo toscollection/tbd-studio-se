12eb25cbeab548f982975a33879977c35b1677cf
Jean-Philippe HAUTIN <25222735+talend-jphautin@users.noreply.github.com>
2020-11-05 18:02:47 +0100

    feat(TBD-11509) : add support for databricks 7.3.x

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/SparkStreamingConstant.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/SparkStreamingConstant.java
@@ -46,6 +46,8 @@ public final class SparkStreamingConstant {
 
     public static final String AZURE_CONFIGURATION_COMPONENT = "tAzureFSConfiguration"; //$NON-NLS-1$
 
+    public static final String AZURE_FS_CONFIGURATION_COMPONENT = "tAzureFSConfiguration"; //$NON-NLS-1$
+
     public static final String SPARK_STREAMING_SPARKCONFIGURATION_LINKEDPARAMETER = "SPARK_CONFIGURATION"; //$NON-NLS-1$
 
     public static final String SPARKCONFIGURATION_IS_LOCAL_MODE_PARAMETER = "SPARK_LOCAL_MODE"; //$NON-NLS-1$
@@ -83,5 +85,7 @@ public final class SparkStreamingConstant {
     public static final String SPARK_SQL_ROW_COMPONENT = "tSqlRow"; //$NON-NLS-1$
 
     public static final String TMODEL_ENCODER_COMPONENT = "tModelEncoder"; //$NON-NLS-1$
+
+    public static final String TOP_BY_COMPONENT = "tTopBy"; //$NON-NLS-1$
 
 }
