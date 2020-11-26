e5b9742b782d1e115363b5b92fc016260a0dff0d
Max <mtuhai@talend.com>
2020-11-26 13:27:57 +0200

    feat(TBD-11035): Support MapR 6.1 with MEP 6.1.1 Static Lightweight Distro - DI / Spark Batch / Spark Streaming (#1543) (#1733) (#1752)

---
+++ main/plugins/org.talend.hadoop.distribution.mapr611/src/org/talend/hadoop/distribution/mapr611/modulegroup/MapR611OjaiModuleGroup.java
@@ -0,0 +1,30 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.hadoop.distribution.mapr611.modulegroup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.mapr611.MapR611Constant;
+
+public class MapR611OjaiModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroups() {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(MapR611Constant.OJAI_MODULE_GROUP.getModuleName());
+        hs.add(dmg);
+        return hs;
+    }
+
+}
