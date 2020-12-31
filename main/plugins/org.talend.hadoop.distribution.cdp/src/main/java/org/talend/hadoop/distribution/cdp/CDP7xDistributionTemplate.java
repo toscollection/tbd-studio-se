77e0bfb87413ead3099f3462b19257e11f5a32a5
Hanna Liashchuk <46442789+hliashchuk@users.noreply.github.com>
2020-12-31 11:41:47 +0200

    fix(TBD-11772):S3 component doesn't work on CDP 7.1.1 (#1774) (#1799)
    * fix(TBD-11772): fixed missing module group for s3
    
    * fix(TBD-11772): missing dependencies

--- main/plugins/org.talend.hadoop.distribution.cdp/src/main/java/org/talend/hadoop/distribution/cdp/CDP7xDistributionTemplate.java
+++ main/plugins/org.talend.hadoop.distribution.cdp/src/main/java/org/talend/hadoop/distribution/cdp/CDP7xDistributionTemplate.java
@@ -269,6 +269,11 @@ public class CDP7xDistributionTemplate extends AbstractDynamicCDPDistributionTem
 	public boolean doSupportAssumeRole() {
 		return true;
 	}
+	
+	@Override
+    public boolean useS3AProperties() {
+        return true;
+    }
 
 	@Override
 	public boolean doSupportAvroDeflateProperties() {
