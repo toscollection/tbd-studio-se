a29613b0c5494f1fe30ffd2d2f8b9fe80482fee2
build-talend-studio-ci <build-studio-ci@talend.com>
2021-02-04 13:38:37 +0100

    fix(TBD-12049):Fix unit tests (#1860)
    Co-authored-by: Laurent BOURGEOIS <lbourgeois@talend.com>

--- test/plugins/org.talend.hadoop.distribution.hdp260.test/src/org/talend/hadoop/distribution/hdp260/test/modulegroup/node/sparkstreaming/HDP260SparkStreamingKafkaAssemblyModuleGroupTest.java
+++ test/plugins/org.talend.hadoop.distribution.hdp260.test/src/org/talend/hadoop/distribution/hdp260/test/modulegroup/node/sparkstreaming/HDP260SparkStreamingKafkaAssemblyModuleGroupTest.java
@@ -12,7 +12,8 @@
 // ============================================================================
 package org.talend.hadoop.distribution.hdp260.test.modulegroup.node.sparkstreaming;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,26 +34,23 @@ public class HDP260SparkStreamingKafkaAssemblyModuleGroupTest {
     public void testModuleGroups() throws Exception {
         Map<String, String> results = new HashMap<String, String>();
 
-        results.put(
-                HDP260Constant.SPARK_KAFKA_ASSEMBLY_MRREQUIRED_MODULE_GROUP.getModuleName(),
-                "(((#LINK@NODE.STORAGE_CONFIGURATION.DISTRIBUTION=='null') AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_VERSION=='null')) AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_LOCAL_MODE=='false') AND (#LINK@NODE.STORAGE_CONFIGURATION.SUPPORTED_SPARK_VERSION=='SPARK_1_6_0'))");
+        results.put(HDP260Constant.SPARK_KAFKA_ASSEMBLY_MRREQUIRED_MODULE_GROUP.getModuleName(),
+                "(((#LINK@NODE.STORAGE_CONFIGURATION.DISTRIBUTION == 'null') AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_VERSION == 'null')) AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_LOCAL_MODE == 'false') AND (#LINK@NODE.STORAGE_CONFIGURATION.SUPPORTED_SPARK_VERSION == 'SPARK_1_6_0'))"); //$NON-NLS-1$
 
-        results.put(
-                HDP260Constant.SPARK2_KAFKA_ASSEMBLY_MRREQUIRED_MODULE_GROUP.getModuleName(),
-                "(((#LINK@NODE.STORAGE_CONFIGURATION.DISTRIBUTION=='null') AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_VERSION=='null')) AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_LOCAL_MODE=='false') AND (#LINK@NODE.STORAGE_CONFIGURATION.SUPPORTED_SPARK_VERSION=='SPARK_2_1_0'))");
+        results.put(HDP260Constant.SPARK2_KAFKA_ASSEMBLY_MRREQUIRED_MODULE_GROUP.getModuleName(),
+                "(((#LINK@NODE.STORAGE_CONFIGURATION.DISTRIBUTION == 'null') AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_VERSION == 'null')) AND (#LINK@NODE.STORAGE_CONFIGURATION.SPARK_LOCAL_MODE == 'false') AND (#LINK@NODE.STORAGE_CONFIGURATION.SUPPORTED_SPARK_VERSION == 'SPARK_2_1_0'))"); //$NON-NLS-1$
 
         Set<DistributionModuleGroup> moduleGroups = HDP260SparkStreamingKafkaAssemblyModuleGroup.getModuleGroups(null, null);
         assertEquals(results.size(), moduleGroups.size());
         moduleGroups.iterator();
         for (DistributionModuleGroup module : moduleGroups) {
-            assertTrue("Should contain module " + module.getModuleName(), results.containsKey(module.getModuleName()));
+            assertTrue("Should contain module " + module.getModuleName(), results.containsKey(module.getModuleName())); //$NON-NLS-1$
             if (results.get(module.getModuleName()) == null) {
-                assertTrue("The condition of the module " + module.getModuleName() + " is not null.",
+                assertTrue("The condition of the module " + module.getModuleName() + " is not null.", //$NON-NLS-1$ //$NON-NLS-2$
                         results.get(module.getModuleName()) == null);
             } else {
-                assertTrue(
-                        "The condition of the module " + module.getModuleName() + " is null, but it should be "
-                                + results.get(module.getModuleName()) + ".", results.get(module.getModuleName()) != null);
+                assertTrue("The condition of the module " + module.getModuleName() + " is null, but it should be " //$NON-NLS-1$ //$NON-NLS-2$
+                        + results.get(module.getModuleName()) + ".", results.get(module.getModuleName()) != null); //$NON-NLS-1$
                 assertEquals(results.get(module.getModuleName()), module.getRequiredIf().getConditionString());
             }
         }
