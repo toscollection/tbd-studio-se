b144e525b0bb6e9c747fa2941f8d53df8e074d1a
sponomarova <42870019+sponomarova@users.noreply.github.com>
2021-02-12 10:01:05 +0200

    fix unit tests (#1873)

--- test/plugins/org.talend.hadoop.distribution.test/src/org/talend/hadoop/distribution/test/hive/HiveMetadataHelper4HortonworksTest.java
+++ test/plugins/org.talend.hadoop.distribution.test/src/org/talend/hadoop/distribution/test/hive/HiveMetadataHelper4HortonworksTest.java
@@ -23,7 +23,7 @@ public class HiveMetadataHelper4HortonworksTest extends AbstractDistributionTest
     // An exhaustive list of all the versions Studio has been supported, some old versions may have been removed.
     private static final String[] VERSIONS_NON_DYNAMIC = new String[] {
     	"Hortonworks Data Platform V3.1.4.12-1 [Built in]",	
-        "Hortonworks Data Platform V2.6.0.3-8 (Deprecated)",
+        "Hortonworks Data Platform V2.6.0.3-8 (Deprecated) [Built in]",
         "Hortonworks Data Platform V2.6.0  (Deprecated)",
         "Hortonworks Data Platform V2.5.0 (Deprecated)"};
 
