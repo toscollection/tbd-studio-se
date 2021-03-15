4d3f956467b5123f4bca2bcf831c083cc5feaada
AlixMetivier <50327620+AlixMetivier@users.noreply.github.com>
2021-03-15 09:49:50 +0100

    fix(TBD-11943): fix snowflake and spark version for dbr73 (#1901)
    * fix(TBD-11943): fix snowflake and spark version for dbr73
    
    * fix
    
    * fix

---
+++ main/plugins/org.talend.hadoop.distribution.dbr73x/src/main/java/org/talend/hadoop/distribution/dbr73x/modulegroup/node/DBR73xSnowflakeNodeModuleGroup.java
@@ -0,0 +1,20 @@
+package org.talend.hadoop.distribution.dbr73x.modulegroup.node;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.dbr73x.DBR73xConstant;
+
+public class DBR73xSnowflakeNodeModuleGroup {
+	
+	public static Set<DistributionModuleGroup> getModuleGroups(String distribution, String version) {
+        Set<DistributionModuleGroup> moduleGroups = new HashSet<>();
+        moduleGroups.add(new DistributionModuleGroup(
+                DBR73xConstant.SNOWFLAKE_GROUP_DBR73x.getModuleName(),
+                true,
+                null
+        ));
+        return moduleGroups;
+    }
+}
