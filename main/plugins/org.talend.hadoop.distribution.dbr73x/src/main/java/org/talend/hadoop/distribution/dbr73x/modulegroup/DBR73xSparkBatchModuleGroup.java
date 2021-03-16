fd3fe70a87e9014827bf295f1a83319716a08056
AlixMetivier <50327620+AlixMetivier@users.noreply.github.com>
2021-03-16 16:27:25 +0100

    fix(TBD-12252): fix DynamoDB with dbr73 (#1909)
    * fix(TBD-12252): fix DynamoDB with dbr73
    
    * fix typo
    
    * fix tests

--- main/plugins/org.talend.hadoop.distribution.dbr73x/src/main/java/org/talend/hadoop/distribution/dbr73x/modulegroup/DBR73xSparkBatchModuleGroup.java
+++ main/plugins/org.talend.hadoop.distribution.dbr73x/src/main/java/org/talend/hadoop/distribution/dbr73x/modulegroup/DBR73xSparkBatchModuleGroup.java
@@ -50,6 +50,11 @@ public class DBR73xSparkBatchModuleGroup {
                 true,
                 notSparkLocal
         ));
+        hs.add(new DistributionModuleGroup(
+                DBR73xConstant.DYNAMODB_GROUP_DBR73x.getModuleName(),
+                true,
+                notSparkLocal
+        ));
         return hs;
     }
 }
