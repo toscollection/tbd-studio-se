bb20b72630c16f0ac481d5791b8b18e7b81299f0
build-talend-studio-ci <build-studio-ci@talend.com>
2021-07-16 16:35:15 +0200

    fix(TBD-12577): added 'Deprecated' to dynamic distro and wizard (#2074)
    Co-authored-by: Max <mtuhai@talend.com>

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/AbstractDynamicDistributionTemplate.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/dynamic/template/AbstractDynamicDistributionTemplate.java
@@ -76,8 +76,11 @@ public abstract class AbstractDynamicDistributionTemplate extends AbstractDistri
         IDynamicPluginConfiguration configuration = pluginAdapter.getPluginConfiguration();
         versionId = configuration.getId();
         versionDisplay = configuration.getName();
-        if ("CDH5xDistributionTemplate".equals(configuration.getTemplateId()) || "HDP2xxDistributionTemplate".equals(configuration.getTemplateId())) {
-        	versionDisplay.replaceAll("Dynamic", "Deprecated");
+        if ("CDH5xDistributionTemplate".equals(configuration.getTemplateId())
+                || "HDP2xxDistributionTemplate".equals(configuration.getTemplateId())
+                || ("HDP3xxDistributionTemplate".equals(configuration.getTemplateId())
+                        && configuration.getVersion().startsWith("3.0"))) {
+            versionDisplay = versionDisplay.replaceAll("Dynamic", "Deprecated");
         }
 
         moduleGroupsTemplateMap = buildModuleGroupsTemplateMap();
