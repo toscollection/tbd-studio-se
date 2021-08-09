9d4ad93626546dfc8601b3bb619c093cedc74a80
hzhao-talendbj <49395568+hzhao-talendbj@users.noreply.github.com>
2021-08-09 23:20:11 +0800

    feat(TUP-27666): remove oozie from talend 8 (#2129)
    * feat(TUP-27666): remove oozie from talend 8
    
    * feat(TUP-27666): remove oozie from talend 8
    
    * feat(TUP-27666): remove oozie from talend 8
    
    * feat(TUP-27666): remove oozie from talend 8

--- main/plugins/org.talend.repository.oozie/src/org/talend/repository/oozie/node/model/OozieRepositoryNodeType.java
+++
@@ -1,9 +0,0 @@
-package org.talend.repository.oozie.node.model;
-
-import org.talend.core.model.repository.ERepositoryObjectType;
-
-public class OozieRepositoryNodeType {
-
-    public static ERepositoryObjectType OOZIE = (ERepositoryObjectType) ERepositoryObjectType.valueOf(
-            ERepositoryObjectType.class, "OOZIE"); //$NON-NLS-1$
-}
