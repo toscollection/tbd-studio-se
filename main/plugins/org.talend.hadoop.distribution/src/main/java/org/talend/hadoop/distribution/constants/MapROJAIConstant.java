244cc0c4cf8b22ff6074caf8b64d31b4bf345f5b
Laurent BOURGEOIS <lbourgeois@talend.com>
2021-10-28 11:02:16 +0200

    fix(TBD-13147):Impossible to import old jobs (#2278) (#2285)

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/MapROJAIConstant.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/constants/MapROJAIConstant.java
@@ -12,6 +12,9 @@
 // ============================================================================
 package org.talend.hadoop.distribution.constants;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class MapROJAIConstant {
 
     public static final String SERVICE = "org.talend.hadoop.distribution.component.MapROJAIComponent";//$NON-NLS-1$
@@ -23,4 +26,6 @@ public final class MapROJAIConstant {
     public static final String VERSION_PARAMETER = "DB_VERSION";//$NON-NLS-1$
 
     public static final String VERSION_REPOSITORYVALUE = "DB_VERSION";//$NON-NLS-1$
+
+    public static final List<String> MAPROJAI_COMPONENTS = Arrays.asList("tMapROjaiInput", "tMapROjaiOutput"); //$NON-NLS-1$ //$NON-NLS-2$
 }
