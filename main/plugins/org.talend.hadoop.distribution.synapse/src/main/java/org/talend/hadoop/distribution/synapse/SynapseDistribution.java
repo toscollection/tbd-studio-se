d4955b540b8b3440f93d121b5fedeada373e44b4
Svitlana Ponomarova <sponomarova@talend.com>
2021-12-29 14:33:06 +0200

    fix(TBD-13285): fix

--- main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
+++ main/plugins/org.talend.hadoop.distribution.synapse/src/main/java/org/talend/hadoop/distribution/synapse/SynapseDistribution.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2020 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -10,11 +10,10 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-
 package org.talend.hadoop.distribution.synapse;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -24,166 +23,121 @@ import org.talend.hadoop.distribution.DistributionModuleGroup;
 import org.talend.hadoop.distribution.EHadoopVersion;
 import org.talend.hadoop.distribution.ESparkVersion;
 import org.talend.hadoop.distribution.NodeComponentTypeBean;
-import org.talend.hadoop.distribution.component.HDFSComponent;
-import org.talend.hadoop.distribution.component.HiveComponent;
-import org.talend.hadoop.distribution.component.HiveOnSparkComponent;
-import org.talend.hadoop.distribution.component.MRComponent;
 import org.talend.hadoop.distribution.component.SparkBatchComponent;
 import org.talend.hadoop.distribution.component.SparkStreamingComponent;
 import org.talend.hadoop.distribution.condition.ComponentCondition;
-import org.talend.hadoop.distribution.constants.ModuleGroupName;
-import org.talend.hadoop.distribution.constants.hdinsight.IMicrosoftHDInsightDistribution;
-import org.talend.hadoop.distribution.spark.SparkClassPathUtils;
 import org.talend.hadoop.distribution.constants.synapse.ISynapseDistribution;
+import org.talend.hadoop.distribution.kafka.SparkStreamingKafkaVersion;
 
-@SuppressWarnings("nls")
-public class SynapseDistribution extends AbstractDistribution implements ISynapseDistribution, 
-			SparkBatchComponent, SparkStreamingComponent, MRComponent {
+public class SynapseDistribution extends AbstractDistribution implements ISynapseDistribution, SparkBatchComponent,
+        SparkStreamingComponent {
 
-    public static final String VERSION = "SYNAPSE";
+	public static final String VERSION = "SYNAPSE";
 
     public static final String VERSION_DISPLAY = "Azure Synapse Runtime for Apache Spark 3.0";
     
     public final static ESparkVersion SPARK_VERSION = ESparkVersion.SPARK_3_0;
 
-	private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*" ;
-	
-	public final static String DEFAULT_LIB_ROOT = "/usr/lib";
-	
-	protected Map<ComponentType, Set<DistributionModuleGroup>> moduleGroups;
+    private final static String YARN_APPLICATION_CLASSPATH = "$HADOOP_CONF_DIR,$HADOOP_COMMON_HOME/*,$HADOOP_COMMON_HOME/lib/*,$HADOOP_HDFS_HOME/*,$HADOOP_HDFS_HOME/lib/*,$HADOOP_MAPRED_HOME/*,$HADOOP_MAPRED_HOME/lib/*,$YARN_HOME/*,$YARN_HOME/lib/*,$HADOOP_YARN_HOME/*,$HADOOP_YARN_HOME/lib/*,$HADOOP_COMMON_HOME/share/hadoop/common/*,$HADOOP_COMMON_HOME/share/hadoop/common/lib/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/*,$HADOOP_HDFS_HOME/share/hadoop/hdfs/lib/*,$HADOOP_YARN_HOME/share/hadoop/yarn/*,$HADOOP_YARN_HOME/share/hadoop/yarn/lib/*" ; //$NON-NLS-1$
 
-	protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> nodeModuleGroups;
+    protected Map<ComponentType, Set<DistributionModuleGroup>> moduleGroups;
 
-	protected Map<ComponentType, ComponentCondition> displayConditions;
+    protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> nodeModuleGroups;
 
-	protected Map<ComponentType, String> customVersionDisplayNames;
+    protected Map<ComponentType, ComponentCondition> displayConditions;
 
-	public SynapseDistribution() {
-		displayConditions = buildDisplayConditions();
+    protected Map<ComponentType, String> customVersionDisplayNames;
+
+    public SynapseDistribution() {
+        displayConditions = buildDisplayConditions();
         customVersionDisplayNames = buildCustomVersionDisplayNames();
         moduleGroups = buildModuleGroups();
-        nodeModuleGroups = buildNodeModuleGroups(getDistribution(), getVersion());        
-	}
+        nodeModuleGroups = buildNodeModuleGroups(getDistribution(), getVersion());
+    }
 
-	protected Map<ComponentType, ComponentCondition> buildDisplayConditions() {
-		return new HashMap<>();
-	}
-
-	protected Map<ComponentType, String> buildCustomVersionDisplayNames() {
+    protected Map<ComponentType, ComponentCondition> buildDisplayConditions() {
         return new HashMap<>();
     }
-	
-	@Override
-	public String getDistribution() {
-		return DISTRIBUTION_NAME;
-	}
 
-	@Override
-	public String getDistributionName() {
-		return DISTRIBUTION_DISPLAY_NAME;
-	}
-
-	@Override
-	public String getVersion() {
-		return VERSION;
-	}
-	
-	@Override
-	public boolean isSynapseDistribution() {
-		return true;
-	}
-
-	@Override
-	public String getVersionName(ComponentType componentType) {
-		return VERSION_DISPLAY;
-	}
-
-	@Override
-	public EHadoopVersion getHadoopVersion() {
-		return EHadoopVersion.HADOOP_3;
-	}
-
-	@Override
-	public boolean doSupportKerberos() {
-		return false;
-	}
-
-	@Override
-	public Set<DistributionModuleGroup> getModuleGroups(
-			ComponentType componentType) {
-		return moduleGroups.get(componentType);
-	}
-
-	@Override
-	public Set<DistributionModuleGroup> getModuleGroups(
-			ComponentType componentType, String componentName) {
-		return nodeModuleGroups.get(new NodeComponentTypeBean(componentType,
-				componentName));
-	}
-
-	@Override
-	public boolean doSupportUseDatanodeHostname() {
-		return false;
-	}
-	
-	@Override
-	public boolean doSupportImpersonation() {
-		return false;
-	}
-	
-	@Override
-	public boolean doSupportCrossPlatformSubmission() {
-		return false;
-	}
-
-	@Override
-	public String getYarnApplicationClasspath() {
-		return YARN_APPLICATION_CLASSPATH;
-	}
-	
-	@Override
-	public String generateSparkJarsPaths(List<String> commandLineJarsPaths, boolean isLightWeight) {
-        return isLightWeight ? "" : generateSparkJarsPaths(commandLineJarsPaths);
+    protected Map<ComponentType, String> buildCustomVersionDisplayNames() {
+        return new HashMap<>();
     }
-
-	@Override
-	public String generateSparkJarsPaths(List<String> commandLineJarsPaths) {
-        return SparkClassPathUtils.generateSparkJarsPaths(commandLineJarsPaths, ModuleGroupName.SPARK_BATCH.get(this.getVersion()));
-    }
-
-	
-	@Override
-    public Set<ESparkVersion> getSparkVersions() {
-		Set<ESparkVersion> version = new HashSet<>();
-        version.add(SynapseDistribution.SPARK_VERSION);
-        return version;
+    
+    @Override
+    public String getDistribution() {
+        return DISTRIBUTION_NAME;
     }
 
     @Override
-    public boolean isExecutedThroughSparkJobServer() {
-        return false;
+    public String getVersion() {
+        return VERSION;
     }
 
-	@Override
-	public boolean doSupportOldImportMode() {
-		return false;
-	}
-	
+    @Override
+    public EHadoopVersion getHadoopVersion() {
+        return EHadoopVersion.HADOOP_3;
+    }
+
+    @Override
+    public String getDistributionName() {
+        return DISTRIBUTION_DISPLAY_NAME;
+    }
+
+    @Override
+    public String getVersionName(ComponentType componentType) {
+        return VERSION_DISPLAY;
+    }
+
+    @Override
+    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType) {
+        return moduleGroups.get(componentType);
+    }
+
+    @Override
+    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType, String componentName) {
+        return nodeModuleGroups.get(new NodeComponentTypeBean(componentType, componentName));
+    }
+
+    @Override
+    public Set<ESparkVersion> getSparkVersions() {
+    	return Collections.singleton(ESparkVersion.SPARK_3_0);
+    }
+
     @Override
     public boolean doSupportSparkStandaloneMode() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public boolean doSupportS3() {
+        return true;
     }
 
     @Override
     public boolean doSupportSparkYarnClientMode() {
         return false;
     }
-    
+
     @Override
-    public boolean doSupportSparkYarnClusterMode() {
-    	return true;
-    };
-    
+    public boolean doSupportAzureDataLakeStorage() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportKerberos() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportS3V4() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAzureDataLakeStorageGen2() {
+        return true;
+    }
+
     @Override
     public boolean doSupportDynamicMemoryAllocation() {
         return true;
@@ -191,36 +145,348 @@ public class SynapseDistribution extends AbstractDistribution implements ISynaps
 
     @Override
     public boolean doSupportCheckpointing() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public boolean doSupportCrossPlatformSubmission() {
+        return true;
     }
 
     @Override
     public boolean doSupportBackpressure() {
-        return false;
-    }
-
-    @Override
-    public short orderingWeight() {
-        return 10;
-    }
-
-    @Override
-    public boolean doSupportLightWeight() {
-        return false;
+        return true;
     }
     
+    @Override
+    public boolean doSupportImpersonation() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportUseDatanodeHostname() {
+        return true;
+    }
+
     @Override
     public boolean isCloudDistribution() {
         return true;
     }
-   
+
     @Override
-    public boolean doSupportAzureBlobStorage() {
+    public boolean isSynapseDistribution() {
         return true;
     }
-    
+
     @Override
-    public boolean doSupportAzureDataLakeStorageGen2() {
+    public boolean useCloudLauncher() {
         return true;
     }
+
+	@Override
+	public boolean doRequireMetastoreVersionOverride() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getHiveMetastoreVersionForSpark() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHiveMetastoreJars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportSparkYarnClusterMode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isExecutedThroughSparkJobServer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isExecutedThroughLivy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isExecutedThroughKnox() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String generateSparkJarsPaths(List<String> commandLineJarsPaths) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generateSparkJarsPaths(List<String> commandLineJarsPaths, boolean isLightWeight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doImportDynamoDBDependencies() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doImportSparkHiveContextDependencies() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean useS3AProperties() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportAssumeRole() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getLightWeightClasspath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isExecutedThroughWebHCat() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportClouderaNavigator() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getYarnApplicationClasspath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportCustomMRApplicationCP() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCustomMRApplicationCP() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportAtlas() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getClouderaNavigatorAPIVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean doSupportBasicAtlasAuthentification() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportAvroDeflateProperties() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean useOldAWSAPI() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getParquetPrefixPackageName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWinUtilsName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isHadoop3() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isHadoop2() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isHadoop1() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportMapRTicket() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportMaprTicketV52API() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportGroup() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ComponentCondition getDisplayCondition(ComponentType componentType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportOldImportMode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDefaultConfig(String... keys) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportHDFSEncryption() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isGoogleDataprocDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAltusDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDatabricksDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCDPDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isQuboleDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportOozie() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportCreateServiceConnection() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> getNecessaryServiceName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isHortonworksDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isActivated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDynamicDistribution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doSupportWebHDFS() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SparkStreamingKafkaVersion getSparkStreamingKafkaVersion(ESparkVersion sparkVersion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean doSupportKerberizedKafka() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean doRequireElasticsearchSparkPatch() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
