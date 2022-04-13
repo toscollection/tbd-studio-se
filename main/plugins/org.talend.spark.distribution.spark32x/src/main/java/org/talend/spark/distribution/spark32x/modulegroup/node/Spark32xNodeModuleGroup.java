1ae01f7d149a60e8659b1153494f58c255c58cfe
AlixMetivier <50327620+AlixMetivier@users.noreply.github.com>
2022-04-13 09:29:41 +0200

    feat(TBD-13185): add spark 3.2.x in local mode (#2562)
    * feat(TBD-13185): add spark 3.2.x in local mode
    
    * update delta version
    
    * add antlr
    
    * fix(TBD-13172): fix avro version
    
    * fix(TBD-13715): fix dynamoDB
    
    * fix(TBD-13714): add gson and failureaccess
    
    * fix(TBD-13716): fix kinesis
    
    * fix(TBD-13715): add gson lib
    
    * fix redshift
    
    * fix(TBD-13759):fix redshift in streaming
    
    * fix

---
+++ main/plugins/org.talend.spark.distribution.spark32x/src/main/java/org/talend/spark/distribution/spark32x/modulegroup/node/Spark32xNodeModuleGroup.java
@@ -0,0 +1,31 @@
+package org.talend.spark.distribution.spark32x.modulegroup.node;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.ESparkVersion;
+import org.talend.hadoop.distribution.condition.ComponentCondition;
+import org.talend.hadoop.distribution.condition.EqualityOperator;
+import org.talend.hadoop.distribution.condition.LinkedNodeExpression;
+import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
+import org.talend.hadoop.distribution.constants.SparkBatchConstant;
+
+public class Spark32xNodeModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroup(String moduleGroupName, String sparkConfigLinkedParameter,
+            ESparkVersion sparkVersion) {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(moduleGroupName, true, spark32xCondition(sparkConfigLinkedParameter, sparkVersion));
+        hs.add(dmg);
+        return hs;
+    }
+
+    static ComponentCondition spark32xCondition(String sparkConfigLinkedParameter, ESparkVersion sparkVersion) {
+        
+        return new SimpleComponentCondition(new LinkedNodeExpression(sparkConfigLinkedParameter,
+                                                                    SparkBatchConstant.VERSION_PARAMETER,//
+                                                                    EqualityOperator.EQ, sparkVersion.getSparkVersion()));
+    }
+    
+}
