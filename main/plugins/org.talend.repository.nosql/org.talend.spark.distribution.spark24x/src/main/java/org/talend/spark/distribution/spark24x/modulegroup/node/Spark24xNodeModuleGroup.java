0ae1f5401840304fe8e6b194dc8cd8959f22a30b
Svitlana Ponomarova <sponomarova@talend.com>
2022-11-03 10:38:01 +0200

    fix(TBD-14425): Compile error for BigQuery in spark batch with universal 3.3x

---
+++ main/plugins/org.talend.repository.nosql/org.talend.spark.distribution.spark24x/src/main/java/org/talend/spark/distribution/spark24x/modulegroup/node/Spark24xNodeModuleGroup.java
@@ -0,0 +1,31 @@
+package org.talend.spark.distribution.spark24x.modulegroup.node;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.talend.hadoop.distribution.DistributionModuleGroup;
+import org.talend.hadoop.distribution.ESparkVersion;
+import org.talend.hadoop.distribution.condition.ComponentCondition;
+import org.talend.hadoop.distribution.condition.EqualityOperator;
+import org.talend.hadoop.distribution.condition.LinkedNodeExpression;
+import org.talend.hadoop.distribution.condition.SimpleComponentCondition;
+import org.talend.hadoop.distribution.constants.SparkBatchConstant;
+
+public class Spark24xNodeModuleGroup {
+
+    public static Set<DistributionModuleGroup> getModuleGroup(String moduleGroupName, String sparkConfigLinkedParameter,
+            ESparkVersion sparkVersion) {
+        Set<DistributionModuleGroup> hs = new HashSet<>();
+        DistributionModuleGroup dmg = new DistributionModuleGroup(moduleGroupName, true, spark24xCondition(sparkConfigLinkedParameter, sparkVersion));
+        hs.add(dmg);
+        return hs;
+    }
+
+    static ComponentCondition spark24xCondition(String sparkConfigLinkedParameter, ESparkVersion sparkVersion) {
+        
+        return new SimpleComponentCondition(new LinkedNodeExpression(sparkConfigLinkedParameter,
+                                                                    SparkBatchConstant.VERSION_PARAMETER,//
+                                                                    EqualityOperator.EQ, sparkVersion.getSparkVersion()));
+    }
+    
+}
