3ad562399ab837d5424265852907b1cdd2db9354
Svitlana Anulich <sanulich@talend.com>
2023-06-16 09:52:10 +0300

    feat(TBD-14991): fix for TBD-15357

--- main/components_lib/talend-bigdata-core-di/src/main/java/org/talend/bigdata/core/di/components/hbase/THbaseTable.java
+++ main/components_lib/talend-bigdata-core-di/src/main/java/org/talend/bigdata/core/di/components/hbase/THbaseTable.java
@@ -1,15 +1,13 @@
 package org.talend.bigdata.core.di.components.hbase;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.NamespaceDescriptor;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.immutables.value.Value;
-
 import java.io.IOException;
 import java.util.*;
-
-import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 
 @Value.Immutable
 @Value.Style(visibility = Value.Style.ImplementationVisibility.PUBLIC)
@@ -60,20 +58,19 @@ public abstract class THbaseTable {
 
     }
 
-    private ColumnFamilyDescriptor getColumnFamily() {
-        ColumnFamilyDescriptor family = null;
+    private HColumnDescriptor getColumnFamily() {
+        HColumnDescriptor family = null;
         for (Map<String, String> map : familyParameters()) {
             String family_name = map.get("FAMILY_NAME");
-            family = new ColumnFamilyDescriptorBuilder.ModifyableColumnFamilyDescriptor(toBytes(family_name));
+            family = new org.apache.hadoop.hbase.HColumnDescriptor(family_name);
         }
         return family;
     }
 
     private void createTable(TableName tableName) throws IOException {
-        TableDescriptorBuilder tableDesBuilder = TableDescriptorBuilder.newBuilder(tableName);
-        ColumnFamilyDescriptor family = getColumnFamily();
-        tableDesBuilder.setColumnFamily(family);
-        org.apache.hadoop.hbase.client.TableDescriptor tableDes = tableDesBuilder.build();
+        HTableDescriptor tableDes = new org.apache.hadoop.hbase.HTableDescriptor(tableName);
+        HColumnDescriptor family = getColumnFamily();
+        tableDes.addFamily(family);
         admin.createTable(tableDes);
     }
 
