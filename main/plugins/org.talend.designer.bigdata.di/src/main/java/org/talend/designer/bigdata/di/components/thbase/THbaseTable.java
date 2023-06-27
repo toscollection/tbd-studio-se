83c1a26c4bc21cb6bdccf681cc4ad71966f3f2ba
Svitlana Anulich <sanulich@talend.com>
2023-06-27 13:43:07 +0300

    feat(TBD-14992): fix TBD-15367 subtask

--- main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/thbase/THbaseTable.java
+++ main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/thbase/THbaseTable.java
@@ -33,6 +33,7 @@ public class THbaseTable extends THbase {
     }
 
     private String updateString(String string){
+        if (string.length() < 2) return string;
         if ((string.charAt(0) == '\"') && (string.charAt(string.length()-1) == '\"')) {
             return string.substring(1,string.length()-1);
         }
