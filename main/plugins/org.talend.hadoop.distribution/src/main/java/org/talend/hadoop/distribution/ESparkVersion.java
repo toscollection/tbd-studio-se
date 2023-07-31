296ea62430ef4048ca7838956e1362b026bc14e3
sponomarova <42870019+sponomarova@users.noreply.github.com>
2023-07-31 10:32:41 +0300

    feat(TBD-15117): add support of universal spark 3.4 (#3380)

--- main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/ESparkVersion.java
+++ main/plugins/org.talend.hadoop.distribution/src/main/java/org/talend/hadoop/distribution/ESparkVersion.java
@@ -22,21 +22,22 @@ import java.util.Map;
  * The enum order is used to determine whether a spark version is later than another.
  */
 public enum ESparkVersion {
-    SPARK_1_3("SPARK_1_3_0", "1.3", "SPARK_130"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_1_4("SPARK_1_4_0", "1.4", "SPARK_140"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_1_5("SPARK_1_5_0", "1.5", "SPARK_150"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_1_6("SPARK_1_6_0", "1.6", "SPARK_160"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_0("SPARK_2_0_0", "2.0", "SPARK_200"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_1("SPARK_2_1_0", "2.1", "SPARK_210"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_2("SPARK_2_2_0", "2.2", "SPARK_220"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_3("SPARK_2_3_0", "2.3", "SPARK_230"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_4("SPARK_2_4_0", "2.4", "SPARK_240"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_3_X("SPARK_2_3_x", "2.3.x", "SPARK_23x"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_2_4_X("SPARK_2_4_x", "Spark 2.4.x", "SPARK_24x"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_3_0("SPARK_3_0_x", "Spark 3.0.x", "SPARK_30x"), //$NON-NLS-1$ //$NON-NLS-2$
-    SPARK_3_1("SPARK_3_1_x", "Spark 3.1.x", "SPARK_31x"), //$NON-NLS-1$ //$NON-NLS-2$
-	SPARK_3_2("SPARK_3_2_x", "Spark 3.2.x", "SPARK_32x"),
-	SPARK_3_3("SPARK_3_3_x", "Spark 3.3.x", "SPARK_33x"); //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_1_3("SPARK_1_3_0", "1.3", "SPARK_130"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_1_4("SPARK_1_4_0", "1.4", "SPARK_140"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_1_5("SPARK_1_5_0", "1.5", "SPARK_150"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_1_6("SPARK_1_6_0", "1.6", "SPARK_160"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_0("SPARK_2_0_0", "2.0", "SPARK_200"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_1("SPARK_2_1_0", "2.1", "SPARK_210"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_2("SPARK_2_2_0", "2.2", "SPARK_220"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_3("SPARK_2_3_0", "2.3", "SPARK_230"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_4("SPARK_2_4_0", "2.4", "SPARK_240"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_3_X("SPARK_2_3_x", "2.3.x", "SPARK_23x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_2_4_X("SPARK_2_4_x", "Spark 2.4.x", "SPARK_24x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_3_0("SPARK_3_0_x", "Spark 3.0.x", "SPARK_30x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_3_1("SPARK_3_1_x", "Spark 3.1.x", "SPARK_31x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_3_2("SPARK_3_2_x", "Spark 3.2.x", "SPARK_32x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_3_3("SPARK_3_3_x", "Spark 3.3.x", "SPARK_33x"), //$NON-NLS-1$ //$NON-NLS-2$
+	SPARK_3_4("SPARK_3_4_x", "Spark 3.4.x", "SPARK_34x"); //$NON-NLS-1$ //$NON-NLS-2$
 
     private static final Map<String, ESparkVersion> lookupBySparkVersion = new HashMap<String, ESparkVersion>();
     private static final Map<String, ESparkVersion> lookupByApiVersion = new HashMap<String, ESparkVersion>();
---
+++ main/plugins/org.talend.spark.distribution.spark34x/src/main/java/org/talend/spark/distribution/spark34x/Spark34xDistribution.java
@@ -0,0 +1,385 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.spark.distribution.spark34x;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.talend.hadoop.distribution.*;
+import org.talend.hadoop.distribution.ComponentType;
+import org.talend.hadoop.distribution.component.*;
+import org.talend.hadoop.distribution.condition.ComponentCondition;
+import org.talend.hadoop.distribution.constants.*;
+import org.talend.hadoop.distribution.constants.apache.ISparkDistribution;
+import org.talend.spark.distribution.spark34x.modulegroup.node.Spark34xNodeModuleGroup;
+
+public class Spark34xDistribution extends AbstractSparkDistribution
+        implements ISparkDistribution, SparkBatchComponent, SparkStreamingComponent, HiveOnSparkComponent, HBaseComponent,
+        HDFSComponent, HCatalogComponent, MRComponent, HiveComponent, ImpalaComponent, SqoopComponent {
+
+
+    public final static ESparkVersion SPARK_VERSION = ESparkVersion.SPARK_3_4;
+   
+    public final static String VERSION = Spark34xDistribution.SPARK_VERSION.getSparkVersion();
+
+    public static final String VERSION_DISPLAY = Spark34xDistribution.SPARK_VERSION.getVersionLabel();
+
+    protected Map<ComponentType, Set<DistributionModuleGroup>> moduleGroups;
+
+    protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> nodeModuleGroups;
+
+    protected Map<ComponentType, ComponentCondition> displayConditions;
+
+    protected Map<ComponentType, String> customVersionDisplayNames;
+
+    public Spark34xDistribution() {
+        this.displayConditions = buildDisplayConditions();
+        this.customVersionDisplayNames = buildCustomVersionDisplayNames();
+        this.moduleGroups = buildModuleGroups();
+        this.nodeModuleGroups = buildNodeModuleGroups(getDistribution(), getVersion());
+    }
+
+    protected Map<ComponentType, ComponentCondition> buildDisplayConditions() {
+        return new HashMap<>();
+    }
+
+    protected Map<ComponentType, String> buildCustomVersionDisplayNames() {
+        Map<ComponentType, String> result = new HashMap<>();
+        return result;
+    }
+
+    @Override
+    protected Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> buildNodeModuleGroups(String distribution,
+            String version) {
+        Map<NodeComponentTypeBean, Set<DistributionModuleGroup>> result = super.buildNodeModuleGroups(distribution, version);
+        Set<DistributionModuleGroup> s3ModuleGroup = Spark34xNodeModuleGroup.getModuleGroup(ModuleGroupName.S3.get(getVersion()),
+                SparkBatchConstant.SPARK_BATCH_S3_SPARKCONFIGURATION_LINKEDPARAMETER, Spark34xDistribution.SPARK_VERSION);
+        result.put(new NodeComponentTypeBean(ComponentType.SPARKBATCH, SparkBatchConstant.S3_CONFIGURATION_COMPONENT),
+                s3ModuleGroup);
+        result.put(new NodeComponentTypeBean(ComponentType.SPARKSTREAMING, SparkBatchConstant.S3_CONFIGURATION_COMPONENT),
+                s3ModuleGroup);
+
+        return result;
+    }
+
+    @Override
+    public String getDistribution() {
+        return DISTRIBUTION_NAME;
+    }
+
+    @Override
+    public String getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public EHadoopVersion getHadoopVersion() {
+        return EHadoopVersion.HADOOP_3;
+    }
+
+    @Override
+    public boolean doSupportKerberos() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSendBySparkSubmit() {
+        return true;
+    }
+
+    @Override
+    public String getDistributionName() {
+        return DISTRIBUTION_DISPLAY_NAME;
+    }
+
+    @Override
+    public String getVersionName(ComponentType componentType) {
+        return VERSION_DISPLAY;
+    }
+
+    @Override
+    public boolean doSupportUseDatanodeHostname() {
+        return true;
+    }
+
+    @Override
+    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType) {
+        return this.moduleGroups.get(componentType);
+    }
+
+    @Override
+    public Set<DistributionModuleGroup> getModuleGroups(ComponentType componentType, String componentName) {
+        return this.nodeModuleGroups.get(new NodeComponentTypeBean(componentType, componentName));
+    }
+
+    @Override
+    public Set<ESparkVersion> getSparkVersions() {
+        Set<ESparkVersion> version = new HashSet<>();
+        version.add(ESparkVersion.SPARK_3_3);
+        return version;
+    }
+
+    @Override
+    public boolean doSupportS3() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportS3V4() {
+        return true;
+    }
+
+    @Override
+    public boolean useOldAWSAPI() {
+        return false;
+    }
+
+    @Override
+    public boolean useS3AProperties() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportSparkStandaloneMode() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportUniversalStandaloneMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportUniversalEMRServerlessMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportSparkYarnClientMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportSparkYarnClusterMode() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportSparkYarnK8SMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportUniversalLocalMode() {
+        return true;
+    }
+    
+    @Override 
+    public boolean doSupportUniversalDataprocMode() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportUniversalDBRMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportUniversalCDEMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportImpersonation() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportCheckpointing() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportBackpressure() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportAzureBlobStorage() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAzureDataLakeStorage() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportAzureDataLakeStorageGen2() {
+        return true;
+    }
+
+    @Override
+    public String getParquetPrefixPackageName() {
+        return EParquetPackagePrefix.APACHE.toString();
+    }
+
+    @Override
+    public boolean doSupportDynamicMemoryAllocation() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportCrossPlatformSubmission() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportOldImportMode() {
+        return false;
+    }
+
+    @Override
+    public String getS3Packages() {
+        return "com.amazonaws:aws-java-sdk-bundle:1.12.170,org.apache.hadoop:hadoop-aws:3.3.4";
+    }
+
+    @Override
+    public String getBlobPackages() {
+        return "org.apache.hadoop:hadoop-azure:3.3.4,com.microsoft.azure:azure-storage:7.0.0";
+    }
+
+    @Override
+    public String getADLS2Packages() {
+        return "org.apache.hadoop:hadoop-azure-datalake:3.3.4,org.apache.hadoop:hadoop-azure:3.3.4";
+    }
+
+    @Override
+    public boolean doSupportAssumeRole() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportHBase2x() {
+        return false;
+    }
+    
+    @Override 
+    public boolean doSupportHBase1x() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportSequenceFileShortType() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportHive1() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportHive2() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportTezForHive() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportHBaseForHive() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportSSL() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportORCFormat() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportAvroFormat() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportParquetFormat() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportStoreAsParquet() {
+        return true;
+    }
+    
+    @Override
+    public boolean doSupportSSLwithKerberos() {
+        return true;
+    }
+
+    @Override
+    public boolean doJavaAPISupportStorePasswordInFile() {
+        return true;
+    }
+
+    @Override
+    public boolean doJavaAPISqoopImportSupportDeleteTargetDir() {
+        return true;
+    }
+
+    @Override
+    public boolean doJavaAPISqoopImportAllTablesSupportExcludeTable() {
+        return true;
+    }
+
+    @Override
+    public boolean doSupportNewHBaseAPI() {
+        return true;
+    }
+    
+    @Override
+    /**
+     * sqoop 1.4.7+ is using apache package
+     */
+    public String getSqoopPackageName() {
+        return ESqoopPackageName.ORG_APACHE_SQOOP.toString();
+    }
+
+    @Override
+    public boolean doSupportUniversalSynapseMode() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportStandaloneMode() {
+        return false;
+    }
+
+    @Override
+    public boolean doSupportEmbeddedMode() {
+        return false;
+    }
+    
+    @Override
+    public boolean doSupportHive1Standalone() {
+        return false;
+    }
+}
