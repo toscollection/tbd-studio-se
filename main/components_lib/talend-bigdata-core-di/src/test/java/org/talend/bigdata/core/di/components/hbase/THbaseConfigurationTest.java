d0e8720f4e8a72e95626678866ff7e8bf4788b7e
build-talend-studio-ci <build-studio-ci@talend.com>
2023-10-12 15:32:37 +0800

    fix(TBD-15695): Null value in entry error when using Impala connection before hbase or other connection (#3494)
    Co-authored-by: Svitlana Anulich <48766596+sanulich@users.noreply.github.com>

--- main/components_lib/talend-bigdata-core-di/src/test/java/org/talend/bigdata/core/di/components/hbase/THbaseConfigurationTest.java
+++ main/components_lib/talend-bigdata-core-di/src/test/java/org/talend/bigdata/core/di/components/hbase/THbaseConfigurationTest.java
@@ -1,31 +1,51 @@
 package org.talend.bigdata.core.di.components.hbase;
 
+import org.apache.hadoop.conf.Configuration;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class THbaseConfigurationTest {
 
     @Test
     public void checkConfiguration() {
-        Map<String, Object> globalMap = new HashMap<>();
-
         ImmutableTHbaseConnection immutableTHbaseConfiguration = ImmutableTHbaseConnection.builder()
-                .globalMap(globalMap)
+                .globalConfiguration(new Configuration())
                 .putHBaseConfigurationOptions("hbase.zookeeper.quorum","localhost")
                 .putHBaseConfigurationOptions("hbase.zookeeper.property.clientPort","2108")
-                .existingConnectionName("")
                 .isUseExistingConnection(false)
                 .build();
 
         immutableTHbaseConfiguration.createConfiguration();
-        immutableTHbaseConfiguration.getConfiguration();
+        Assertions.assertThat(immutableTHbaseConfiguration.getConfiguration()).isNotEmpty();
 
+        Optional<Configuration> configuration = Optional.ofNullable(new Configuration());
+        immutableTHbaseConfiguration = ImmutableTHbaseConnection.builder()
+                .globalConfiguration(configuration)
+                .putHBaseConfigurationOptions("hbase.zookeeper.quorum","localhost")
+                .putHBaseConfigurationOptions("hbase.zookeeper.property.clientPort","2108")
+                .isUseExistingConnection(true)
+                .build();
+
+        immutableTHbaseConfiguration.createConfiguration();
         Assertions.assertThat(immutableTHbaseConfiguration.getConfiguration()).isNotEmpty();
 
     }
+    @Test
+    public void configurationIsNull() {
+        Optional<Configuration> configuration = Optional.ofNullable(null);
+        ImmutableTHbaseConnection immutableTHbaseConfiguration = ImmutableTHbaseConnection.builder()
+                .globalConfiguration(configuration)
+                .putHBaseConfigurationOptions("hbase.zookeeper.quorum","localhost")
+                .putHBaseConfigurationOptions("hbase.zookeeper.property.clientPort","2108")
+                .isUseExistingConnection(true)
+                .build();
 
+        Throwable exception = assertThrows(RuntimeException.class, immutableTHbaseConfiguration::createConfiguration);
+        assertEquals("Configuration for HBase connection is null", exception.getMessage());
+    }
 
 }
