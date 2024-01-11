0818901d44e982f8cd5774093376b82d8d6fa13b
build-talend-studio-ci <build-studio-ci@talend.com>
2024-01-11 04:17:25 +0100

    feat(TBD-15753): support add/drop/change/column action (#3665)
    * feat(TBD-15753): support add/drop/change/column action
    
    * feat(TBD-15753): suport reorder column action
    
    * feat(TBD-15753): upgrade version to 1.8.0 of org.talend.bigdata.core.di
    
    * feat(TBD-15753): update UI
    
    * feat(TBD-15753): modify UI side
    
    * wip
    
    * wip
    
    * feat(TBD-15753): update backgroup to support new UI
    
    * feat(TBD-15753): support add/drop/change/column action
    
    * feat(TBD-15753): suport reorder column action
    
    * feat(TBD-15753): update UI
    
    * feat(TBD-15753): modify UI side
    
    * feat(TBD-15753): update backgroup to support new UI
    
    * feat(TBD-15753): change reOrder column after as text
    
    * feat(TBD-15753): improve code as review request
    
    * feat(TBD-15753): improve code for review request
    
    ---------
    
    Co-authored-by: zshen-talend <zshen@talend.com>
    Co-authored-by: Laurent BOURGEOIS <lbourgeois@talend.com>

--- main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/iceberg/TIcebergTable.java
+++ main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/iceberg/TIcebergTable.java
@@ -1,5 +1,6 @@
 package org.talend.designer.bigdata.di.components.iceberg;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -7,11 +8,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.talend.core.model.metadata.IMetadataTable;
 import org.talend.core.model.process.INode;
 import org.talend.core.model.utils.NodeUtil;
 import org.talend.designer.bigdata.di.components.DesignerDIComponent.WithSchema;
 import org.talend.designer.bigdata.di.components.Schema;
+import org.talend.designer.bigdata.di.components.Schema.Field;
 import org.talend.designer.codegen.config.CodeGeneratorArgument;
 
 public class TIcebergTable implements WithSchema {
@@ -21,15 +24,55 @@ public class TIcebergTable implements WithSchema {
     private final static Map<String, String> tableActionToMethod = new HashMap<String, String>();
 
     static {
+        tableActionToMethod.put("CHANGE_COLUMNS", "changeColumn()");
         tableActionToMethod.put("CREATE_TABLE", "create()");
         tableActionToMethod.put("CREATE_TABLE_IF_NOT_EXISTS", "create(true)");
+        tableActionToMethod.put("DROP_COLUMNS", "dropColumns()");
         tableActionToMethod.put("DROP_TABLE", "drop()");
         tableActionToMethod.put("DROP_TABLE_IF_EXISTS", "drop(true)");
         tableActionToMethod.put("TRUNCATE", "truncate()");
+        tableActionToMethod.put("SET_PARTITIONS", "alterPartitions()");
+        tableActionToMethod.put("ADD_COLUMNS", "addColumns()");
     }
 
     public TIcebergTable(CodeGeneratorArgument arg) {
         this.codeGeneratorArgument = arg;
+    }
+
+    public boolean isChangeColumnAction() {
+        return "CHANGE_COLUMNS".equals(getActionName());
+    }
+
+    @Override
+    public List<Field> getFields() {
+        if (isChangeColumnAction()) {
+            ArrayList<Map<String, String>> mappingTable = getColumnsMappingTable();
+            INode node = (INode) codeGeneratorArgument().getArgument();
+            return node.getMetadataList()
+                    .stream()
+                    .findFirst()
+                    .get()
+                    .getListColumns()
+                    .stream()
+                    .filter(field -> {
+                        for (Map<String, String> tableItem : mappingTable) {
+                            if (field.getLabel().equals(tableItem.get("TARGET_COLUMN"))) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    })
+                    .map(c -> new Schema.Field(c.getLabel(), Schema.StudioToJavaType(c.getTalendType())))
+                    .collect(Collectors.toList());
+        } else {
+            return WithSchema.super.getFields();
+        }
+    }
+
+    protected ArrayList<Map<String, String>> getColumnsMappingTable() {
+        ArrayList<Map<String, String>> mappingTable =
+                getParameter("__COLUMN_MAPPING__", new ArrayList<Map<String, String>>());
+        return mappingTable;
     }
 
     @Override
@@ -45,8 +88,16 @@ public class TIcebergTable implements WithSchema {
         return getParameter("__TABLE__", "");
     }
 
+    public String getActionName() {
+        String actionName = getParameter("__TABLE_ACTION__", "CREATE_TABLE");
+        if ("ALTER".equals(actionName)) {
+            return getParameter("__ALTER_TABLE_ACTION__", "ADD_COLUMNS");
+        }
+        return actionName;
+    }
+
     public String actionMethod() {
-        return tableActionToMethod.get(getParameter("__TABLE_ACTION__", "CREATE_TABLE"));
+        return tableActionToMethod.get(getActionName());
     }
 
     public boolean externalTable() {
@@ -85,6 +136,10 @@ public class TIcebergTable implements WithSchema {
                 : Collections.emptyList();
     }
 
+    public List<String> newPartitions() {
+        return getParameter("__NEW_PARTITIONS__", Collections.<Map<String, String>> emptyList()).stream().map(p -> p.get("PARTITION")).collect(Collectors.toList());
+    }
+
     public Map<String, String> tableProperties() {
         Map<String, String> tableProperties = new HashMap<String, String>();
         getParameter("__TABLE_PROPERTIES__", Collections.<Map<String, String>> emptyList()).stream().forEach(p -> tableProperties.put(p.get("PROPERTY"), p.get("VALUE")));
@@ -102,4 +157,38 @@ public class TIcebergTable implements WithSchema {
     public boolean purge() {
         return getBooleanParameter("__PURGE__", false);
     }
+
+    public String getRenameCol(Schema.Field targetField) {
+        ArrayList<Map<String, String>> mappingTable = getColumnsMappingTable();
+        for (Map<String, String> tableItem : mappingTable) {
+            if (targetField.getName().equals(tableItem.get("TARGET_COLUMN"))) {
+                return StringUtils.isEmpty(tableItem.get("RENAME")) ? targetField.getName() : tableItem.get("RENAME");
+            }
+
+        }
+        return targetField.getName();
+    }
+
+    public String getType(Schema.Field targetField) {
+        ArrayList<Map<String, String>> mappingTable = getColumnsMappingTable();
+        for (Map<String, String> tableItem : mappingTable) {
+            if (targetField.getName().equals(tableItem.get("TARGET_COLUMN"))) {
+                return StringUtils.isEmpty(tableItem.get("TYPE")) ? targetField.getType() : tableItem.get("TYPE");
+            }
+
+        }
+        return targetField.getType();
+    }
+
+    public String getAfterCol(Schema.Field targetField) {
+        ArrayList<Map<String, String>> mappingTable = getColumnsMappingTable();
+        for (Map<String, String> tableItem : mappingTable) {
+            if (targetField.getName().equals(tableItem.get("TARGET_COLUMN"))) {
+                return tableItem.get("AFTER");
+            }
+
+        }
+        return "";
+    }
+
 }
