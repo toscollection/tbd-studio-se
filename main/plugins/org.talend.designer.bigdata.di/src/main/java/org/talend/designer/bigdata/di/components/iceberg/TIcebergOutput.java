45bc2a6ca131225f6892c4ca910978305b81be78
build-talend-studio-ci <build-studio-ci@talend.com>
2024-01-30 12:32:33 +0100

    feat(TBD-15770): support insert overwrite ticebergOutput (#3698)
    * feat(TBD-15770): support insert overwrite for ticebergOutput
    
    * feat(TBD-15770): modify to improve UI
    
    * feat(TBD-15770): change name and upgrade library
    
    * feat(TBD-15770): fix source table no show issue
    
    Co-authored-by: zshen-talend <zshen@talend.com>

--- main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/iceberg/TIcebergOutput.java
+++ main/plugins/org.talend.designer.bigdata.di/src/main/java/org/talend/designer/bigdata/di/components/iceberg/TIcebergOutput.java
@@ -1,11 +1,21 @@
 package org.talend.designer.bigdata.di.components.iceberg;
 
+import org.talend.core.model.process.EConnectionType;
+import org.talend.core.model.process.INode;
 import org.talend.designer.bigdata.di.components.DesignerDIComponent;
 import org.talend.designer.codegen.config.CodeGeneratorArgument;
 
 public class TIcebergOutput implements DesignerDIComponent.WithOneInput, DesignerDIComponent.WithSchema, DesignerDIComponent.WithConnection {
 
     protected final CodeGeneratorArgument codeGeneratorArgument;
+
+    public boolean hasMainFlow() {
+        INode node = (INode) codeGeneratorArgument().getArgument();
+        return node.getIncomingConnections(EConnectionType.FLOW_MAIN)
+                .stream()
+                .findFirst()
+                .isPresent();
+    }
 
     public TIcebergOutput(CodeGeneratorArgument arg) {
         this.codeGeneratorArgument = arg;
@@ -20,8 +30,28 @@ public class TIcebergOutput implements DesignerDIComponent.WithOneInput, Designe
         return getParameter("__TABLE__", "");
     }
 
+    public String getSourceTable() {
+        return getParameter("__SOURCE_TABLE_NAME__", "");
+    }
+
+    public String getCustomQuery() {
+        return getParameter("__CUSTOM_QUERY__", "");
+    }
+
     public boolean useBatch() {
         return getBooleanParameter("__USE_BATCH__", false);
+    }
+
+    public boolean useInsertOverwrite() {
+        return getBooleanParameter("__USE_INSERT_OVERWRITE__", false);
+    }
+
+    public boolean useSourceTable() {
+        return getBooleanParameter("__ALL_ROWS_FROM_SOURCE_TABLE__", false);
+    }
+
+    public boolean useCustomQuery() {
+        return getBooleanParameter("__USE_A_CUSTOM_QUERY__", false);
     }
 
     public int batchSize() {
